eadc340ad3b0b60f79f8f125467914e79f8f54d1
dizzib <dizzib.com@gmail.com>
2024-07-31 14:38:09 +0100

    refactor

--- lib/view/feeds_view.dart
+++ lib/view/feeds_view.dart
@@ -6,39 +6,30 @@ import 'all_done.dart';
 import 'feed_card.dart';
 
 class FeedsView extends StatelessWidget {
-  // cache body (do not rebuild on update)
-  // Columns and Rows work better than a GridView
-  final _body = Column(
-    children: [
-      for (int index in [0, 2, 4, 6, 8])
-      Expanded(
-        child: Row(
-          children: [
-            Expanded(
-              child: ChangeNotifierProvider.value(
-                value: feeds[index],
-                child: FeedCard()
-              ),
-            ),
-            Expanded(
-              child: ChangeNotifierProvider.value(
-                value: feeds[index + 1],
-                child: FeedCard()
-              ),
-            ),
-          ],
-        ),
-      )
-    ]
-  );
-
   @override
   Widget build(BuildContext context) {
     context.watch<Feeds>();
     Navigator.maybePop(context);  // dismiss possible dialog (originator might be cron)
 
+    feedCard(int index) =>
+      Expanded(
+        child: ChangeNotifierProvider.value(
+          value: feeds[index],
+          child: FeedCard()
+        ),
+      );
+
     return Scaffold(
-      body: _body,
+      body: Column(  // Columns and Rows work better than a GridView
+        children: [
+          for (int index in [0, 2, 4, 6, 8])
+          Expanded(
+            child: Row(
+              children: [feedCard(index), feedCard(index + 1)]
+            )
+          )
+        ]
+      ),
       floatingActionButton: feeds.areChaptersRead ? AllDone() : null,  // null activates animation
       floatingActionButtonLocation: FloatingActionButtonLocation.centerTop,
     );
