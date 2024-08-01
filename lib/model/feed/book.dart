ae2cbafa7fef39223bcf9c956141b1075343de04
dizzib <dizzib.com@gmail.com>
2024-08-01 16:16:17 +0100

    refactor
    .

--- lib/model/feed/book.dart
+++ lib/model/feed/book.dart
@@ -2,29 +2,36 @@ part of '../feed.dart';
 
 // an individual book e.g. Matthew, with reading state
 class Book {
+  final String key;  // e.g. mat
+  final String _name;  // e.g. Matthew
+  final List<int> _chapters;
+
   Book(
     this.key,
     this._name,
     int chapterCount
   ) : _chapters = List<int>.generate(chapterCount, (n) => n);
 
-  // private
-  final List<int> _chapters;
+  // state
   int _index = 0;
   bool _isChapterRead = false;
-  final String _name;  // e.g. Matthew
+
+  // private
   void _reset() { _index = 0; _isChapterRead = false; }
 
   // testing
-  @visibleForTesting set chapter(int c) => _index = c - 1;
-  @visibleForTesting int get chaptersRead => _index + (isChapterRead ? 1 : 0);
+  @visibleForTesting set chapter(int val) => _index = val - 1;
+  @visibleForTesting get chaptersRead => _index + (isChapterRead ? 1 : 0);
   @visibleForTesting set isChapterRead(bool val) => _isChapterRead = val;
-  @visibleForTesting final String key;  // e.g. mat
-  @visibleForTesting void nextChapter() { assert(_isChapterRead); _index = ++_index % count; isChapterRead = false; }
+  @visibleForTesting void nextChapter() {
+    assert(_isChapterRead);
+    _index = ++_index % count;
+    isChapterRead = false;
+  }
 
   // public
   int get chapter => 1 + _index;
   int get count => _chapters.length;
   bool get isChapterRead => _isChapterRead;
-  String get name => _name;
+  get name => _name;
 }
--- lib/model/feed/books.dart
+++ lib/model/feed/books.dart
@@ -2,32 +2,33 @@ part of '../feed.dart';
 
 // books in a reading list (e.g. Matthew, Mark, Luke and John) with state
 class Books {
+  final String _key;  // e.g. gos
+  final String _name;  // e.g. Gospels
+  final List<Book> _bookList;
+  final int _totalChapters;
+
   Books(
     this._key,
     this._name,
     this._bookList
   ) : _totalChapters = _bookList.fold(0, (t, b) => t + b.count);
 
-  // private
-  final List<Book> _bookList;
+  // state
   int _index = 0;
-  final String _key;  // e.g. gos
-  final String _name;  // e.g. Gospels
-  final int _totalChapters;
 
   // testing
   @visibleForTesting set current(Book b) => _index = indexOf(b);
-  @visibleForTesting Book getBook(String key) => _bookList.where((Book b) => b.key == key).single;
-  @visibleForTesting void nextBook() { _index = ++_index % count; }
+  @visibleForTesting getBook(String key) => _bookList.where((Book b) => b.key == key).single;
+  @visibleForTesting nextBook() { _index = ++_index % count; }
 
   // public
-  Book operator [](int i) => _bookList[i];
-  int chaptersTo(Book b, int chapter) => _bookList.sublist(0, indexOf(b)).fold(0, (t, b) => t + b.count) + chapter;
+  operator [](int i) => _bookList[i];
+  chaptersTo(Book b, int chapter) => _bookList.sublist(0, indexOf(b)).fold(0, (t, b) => t + b.count) + chapter;
   int get count => _bookList.length;  // e.g. 4 books in The Gospels
   Book get current => _bookList[_index];
-  int indexOf(Book b) => _bookList.indexOf(b);
-  String get name => _name;
-  double get progress => progressTo(current, current.chaptersRead);
-  double progressTo(Book b, int chapter) => chaptersTo(b, chapter) / _totalChapters;
-  int get totalChapters => _totalChapters;
+  indexOf(Book b) => _bookList.indexOf(b);
+  get name => _name;
+  get progress => progressTo(current, current.chaptersRead);
+  progressTo(Book b, int chapter) => chaptersTo(b, chapter) / _totalChapters;
+  get totalChapters => _totalChapters;
 }
