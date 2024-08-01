90524765d2186aed1f37fbc3bb9bea244c638cf2
dizzib <dizzib.com@gmail.com>
2024-08-01 17:15:42 +0100

    refactor

--- lib/model/feed/books.dart
+++ lib/model/feed/books.dart
@@ -5,13 +5,12 @@ class Books {
   final String key;  // e.g. gos
   final String name;  // e.g. Gospels
   final List<Book> _bookList;
-  final int _totalChapters;
+  final int count;  // e.g. 4 books in The Gospels
+  final int totalChapters;
 
-  Books(
-    this.key,
-    this.name,
-    this._bookList
-  ) : _totalChapters = _bookList.fold(0, (t, b) => t + b.count);
+  Books(this.key, this.name, this._bookList) :
+    totalChapters = _bookList.fold(0, (t, b) => t + b.count),
+    count = _bookList.length;
 
   // state
   int _index = 0;
@@ -24,10 +23,8 @@ class Books {
   // public
   operator [](int i) => _bookList[i];
   chaptersTo(Book b, int chapter) => _bookList.sublist(0, indexOf(b)).fold(0, (t, b) => t + b.count) + chapter;
-  int get count => _bookList.length;  // e.g. 4 books in The Gospels
   Book get current => _bookList[_index];
   indexOf(Book b) => _bookList.indexOf(b);
   get progress => progressTo(current, current.chaptersRead);
-  progressTo(Book b, int chapter) => chaptersTo(b, chapter) / _totalChapters;
-  get totalChapters => _totalChapters;
+  progressTo(Book b, int chapter) => chaptersTo(b, chapter) / totalChapters;
 }
