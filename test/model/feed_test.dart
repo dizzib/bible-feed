0cde96bf155548220d462533a18e089942b7b90f
dizzib <dizzib.com@gmail.com>
2024-08-02 15:35:47 +0100

    refactor

--- test/model/feed_test.dart
+++ test/model/feed_test.dart
@@ -3,6 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:bible_feed/data/store.dart';
 import 'package:bible_feed/model/feed.dart';
 
+// helpers
 extension MyDateExtension on DateTime { DateTime get date { return DateTime(year, month, day); } }
 
 void main() {
@@ -12,7 +13,7 @@ void main() {
   setUp(() async {
     SharedPreferences.setMockInitialValues({
       'fd0.book': 'b1',
-      'fd0.chapter': 3,
+      'fd0.chapter': 2,
       'fd0.isChapterRead': true,
       'fd0.dateLastSaved': '2024-05-10T14:33:25.470094',
     });
@@ -24,44 +25,55 @@ void main() {
     f = Feed(Books('fd0', 'Book list', [b0, b1]));
   });
 
-  // helpers
-  DateTime getDateLastSaved() => DateTime.parse(Store.getString('fd0.dateLastSaved')!);
+  // store helpers
+  String getStoredBookKey() => Store.getString('fd0.book')!;
+  int getStoredChapter() => Store.getInt('fd0.chapter')!;
+  bool getStoredIsChapterRead() => Store.getBool('fd0.isChapterRead')!;
+  DateTime getStoredDateLastSaved() => DateTime.parse(Store.getString('fd0.dateLastSaved')!);
+
+  // test helpers
+  void checkBookChapterAndStore(Book expectedBook, int expectedChapter) {
+    expect(f.books.current, expectedBook);
+    expect(f.books.current.chapter, expectedChapter);
+    expect(getStoredBookKey(), expectedBook.key);
+    expect(getStoredChapter(), expectedChapter);
+    expect(getStoredDateLastSaved().date, DateTime.now().date);
+  }
 
   test('constructor should load state from store', () {
     expect(f.books.current, b1);
-    expect(f.dateLastSaved, getDateLastSaved());
-    expect(b1.chapter, 3);
+    expect(f.dateLastSaved, getStoredDateLastSaved());
+    expect(b1.chapter, 2);
     expect(b1.isChapterRead, true);
   });
 
-  group('nextChapter', () {
-    test('if read, should move to next book and save state to store', () {
+  group('nextChapter, if read', () {
+    test('non-last chapter, should remain on current book and save state to store', () {
       f.nextChapter();
-      expect(f.books.current, b0);
-      expect(b0.chapter, 1);
-      expect(Store.getString('fd0.book'), 'b0');
-      expect(Store.getInt('fd0.chapter'), 1);
-      expect(getDateLastSaved().date, DateTime.now().date);
+      checkBookChapterAndStore(b1, 3);
+    });
+
+    test('last chapter, should move to next book and save state to store', () {
+      f.nextChapter();
+      f.toggleIsChapterRead();
+      f.nextChapter();
+      checkBookChapterAndStore(b0, 1);
     });
   });
 
   test('setBookAndChapter should reset current and save state to store', () {
     f.setBookAndChapter(b0, 4);
-    expect(f.books.current, b0);
-    expect(b0.chapter, 4);
+    checkBookChapterAndStore(b0, 4);
     expect(b1.isChapterRead, false);
     expect(b1.chapter, 1);
-    expect(Store.getString('fd0.book'), 'b0');
-    expect(Store.getInt('fd0.chapter'), 4);
-    expect(getDateLastSaved().date, DateTime.now().date);
   });
 
   test('toggleIsChapterRead should toggle and save state to store', () {
     run(bool expectIsRead) {
       f.toggleIsChapterRead();
       expect(b1.isChapterRead, expectIsRead);
-      expect(Store.getBool('fd0.isChapterRead'), expectIsRead);
-      expect(getDateLastSaved().date, DateTime.now().date);
+      expect(getStoredIsChapterRead(), expectIsRead);
+      expect(getStoredDateLastSaved().date, DateTime.now().date);
     }
     run(false);
     run(true);
