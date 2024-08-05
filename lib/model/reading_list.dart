10666d9b62ccadc82905fe7c75ba8938a1d78945
dizzib <dizzib.com@gmail.com>
2024-08-05 10:57:55 +0100

    mark as @immutable

--- lib/model/reading_list.dart
+++ lib/model/reading_list.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'book.dart';
 
 extension ListExtension on List<Book> { int get totalChapters => fold(0, (t, b) => t + b.chapterCount); }
 
 // books in a reading list (e.g. Matthew, Mark, Luke and John) without state
+@immutable
 final class ReadingList {
   final int count;  // e.g. 4 books in The Gospels
   final String key;  // e.g. gos
