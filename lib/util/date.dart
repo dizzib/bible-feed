093fadee364300c225be8d8fd1e11bf21a98df08
dizzib <dizzib.com@gmail.com>
2024-08-05 23:11:22 +0100

    add comment

--- lib/util/date.dart
+++ lib/util/date.dart
@@ -6,7 +6,7 @@ extension DateHelper on DateTime {
   DateTime addDays(int days) => add(Duration(days: days));
 
   bool get isToday {
-    final now = clock.now();
+    final now = clock.now();  // use clock (not DateTime) for unit testing
     return now.day == day && now.month == month && now.year == year;
   }
 }
