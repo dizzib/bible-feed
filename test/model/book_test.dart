4aa9a67663fcdc4dbd64ea8b360e30b569d30663
dizzib <dizzib.com@gmail.com>
2024-08-07 01:27:28 +0100

    refactor

--- test/model/book_test.dart
+++ test/model/book_test.dart
@@ -1,12 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:bible_feed/model/book.dart';
+import '_test_data.dart';
 
 void main() {
-  var b = const Book('bk', 'Book', 3);
-
   test('constructor', () {
-    expect(b.chapterCount, 3);
-    expect(b.key, 'bk');
-    expect(b.name, 'Book');
+    expect(b0.chapterCount, 5);
+    expect(b0.key, 'b0');
+    expect(b0.name, 'Book 0');
   });
 }
--- test/model/reading_list_test.dart
+++ test/model/reading_list_test.dart
@@ -1,45 +1,40 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:bible_feed/model/book.dart';
-import 'package:bible_feed/model/reading_list.dart';
+import '_test_data.dart';
 
 void main() {
-  var b0 = const Book('b0', 'Book0', 5);
-  var b1 = const Book('b1', 'Book1', 3);
-  var b2 = const Book('b2', 'Book2', 2);
-  var l = ReadingList('bks0', 'My book list', [b0, b1, b2]);
 
   test('constructor', () {
-    expect(l.count, 3);
-    expect(l.totalChapters, 10);
+    expect(l2.count, 3);
+    expect(l2.totalChapters, 10);
   });
 
   test('[]', () {
-    expect(l[0], b0);
-    expect(l[1], b1);
-    expect(l[2], b2);
+    expect(l2[0], b0);
+    expect(l2[1], b1);
+    expect(l2[2], b2);
   });
 
   test('chaptersTo', () {
-    expect(l.chaptersTo(0, 0), 0);
-    expect(l.chaptersTo(1, 1), 6);
-    expect(l.chaptersTo(2, 2), 10);
+    expect(l2.chaptersTo(0, 0), 0);
+    expect(l2.chaptersTo(1, 1), 6);
+    expect(l2.chaptersTo(2, 2), 10);
   });
 
   test('getBook', () {
-    expect(l.getBook('b0'), b0);
-    expect(l.getBook('b1'), b1);
-    expect(l.getBook('b2'), b2);
+    expect(l2.getBook('b0'), b0);
+    expect(l2.getBook('b1'), b1);
+    expect(l2.getBook('b2'), b2);
   });
 
   test('indexOf', () {
-    expect(l.indexOf(b0), 0);
-    expect(l.indexOf(b1), 1);
-    expect(l.indexOf(b2), 2);
+    expect(l2.indexOf(b0), 0);
+    expect(l2.indexOf(b1), 1);
+    expect(l2.indexOf(b2), 2);
   });
 
   test('progressTo', () {
-    expect(l.progressTo(0, 0), 0.0);
-    expect(l.progressTo(1, 1), 0.6);
-    expect(l.progressTo(2, 2), 1.0);
+    expect(l2.progressTo(0, 0), 0.0);
+    expect(l2.progressTo(1, 1), 0.6);
+    expect(l2.progressTo(2, 2), 1.0);
   });
 }
