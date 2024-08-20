fea2eafeedd6f9fbbaccefac115b769461714ef8
dizzib <dizzib.com@gmail.com>
2024-08-20 02:15:28 +0100

    improve logging

--- lib/util/log.dart
+++ lib/util/log.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/foundation.dart';
 
-extension LogExtension on Object {
+class AnsiColor {
+  static const reset = '\x1B[0m';
+  static const red = '\x1B[31m';
+  static const green = '\x1B[32m';
+  static const yellow = '\x1B[33m';
+  static const blue = '\x1B[34m';
+  static const cyan = '\x1B[36m';
+}
+
+extension LogObjExt on Object {
   void log() {
-    if(kDebugMode) debugPrint('${DateTime.now().toString()} ${toString()}');
+    if(kDebugMode) {
+      debugPrint('${AnsiColor.yellow}${DateTime.now().toString()} ${AnsiColor.cyan}${toString()}${AnsiColor.reset}');
+    }
   }
 }
