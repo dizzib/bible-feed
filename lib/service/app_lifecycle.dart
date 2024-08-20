6ea3a6ccd77f137170e6d731237bf78d77212cc9
dizzib <dizzib.com@gmail.com>
2024-08-20 07:36:03 +0100

    fix bug

--- lib/service/app_lifecycle.dart
+++ lib/service/app_lifecycle.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:watch_it/watch_it.dart';
 import '../model/feeds.dart';
-import '../util/log.dart';
 
 class AppLifecycle {
   AppLifecycle() {
     AppLifecycleListener(onResume: () {
-      'onResume'.log();
-      di<Feeds>().maybeAdvance;
+      di<Feeds>().maybeAdvance();
     });
   }
 }
