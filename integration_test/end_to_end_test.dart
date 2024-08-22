572cedaa043a39823c3a659087feb2313d508301
dizzib <dizzib.com@gmail.com>
2024-08-22 22:49:04 +0100

    improve end-to-end test

--- integration_test/end_to_end_test.dart
+++ integration_test/end_to_end_test.dart
@@ -5,15 +5,16 @@ import 'helper.dart';
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   testWidgets('end-to-end', (WidgetTester t) async {
-    await initialiseApp(t);
-    expectChapters(1);
-    await markAllListsAsRead(t);
-    await advanceManually(t);
-    expectChapters(2);
-    await selectLastBookAndChapter(t, 'Gospels', 'John', 21);
-    expectChapters(2, count:9);
-    await markAllListsAsRead(t);
-    expect(find.text('All done!'), findsNothing);  // 2nd time
-    // await advanceManually(t);
+    await t.initialiseApp(); expectChapters(1);
+    await t.tapAllLists();
+    await t.tapNo(); expectChapters(1);
+    await t.tapFab();
+    await t.tapYes(); expectChapters(2);
+    await t.tapAllLists(); expect(find.text('All done!'), findsNothing);  // 2nd time
+    await t.tapFab();
+    await t.tapYes(); expectChapters(3);
+    await t.selectLastBookAndChapter('Gospels');
+    expectBookAndChapter('John', 21);
+    expectChapters(3, count:9);
   });
 }
--- integration_test/helper.dart
+++ integration_test/helper.dart
@@ -2,37 +2,61 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:bible_feed/main.dart' as bible_feed;
 import 'package:bible_feed/view/feed_card.dart';
+import 'package:bible_feed/util/log.dart';
 
-advanceManually(WidgetTester t) async {
-  expect(find.text('All done!'), findsOneWidget);
-  await t.tap(find.text('Yes'));
-  await t.pumpAndSettle();
+expectBookAndChapter(String expectedBookName, int expectedChapter) {
+  expect(find.text(expectedBookName), findsOneWidget);
+  expectChapters(expectedChapter, count:1);
 }
 
 expectChapters(int expectedValue, {int count = 10}) {
   expect(find.text(expectedValue.toString()), findsExactly(count));
 }
 
-initialiseApp(WidgetTester t) async {
-  await bible_feed.main();
-  await t.pumpAndSettle();
-}
+extension Helper on WidgetTester {
+  initialiseApp() async {
+    await bible_feed.main();
+    await pumpAndSettle();
+  }
 
-markAllListsAsRead(WidgetTester t) async {
-  var feedCards = find.byType(FeedCard).evaluate();
-  for (var el in feedCards) { await t.tapAt(t.getCenter(find.byWidget(el.widget))); }
-  await t.pumpAndSettle();
-}
+  selectLastBookAndChapter(String feedName) async {
+    await longPress(find.text(feedName));
+    await pumpAndSettle();
+    await drag(find.byKey(const Key('book_wheel')), const Offset(0, -999));  // last book
+    await drag(find.byKey(const Key('chapter_wheel')), const Offset(0, -999));  // last chapter
+    await pumpAndSettle();
+    await tap(find.text('Update'));
+    await pumpAndSettle();
+  }
 
-selectLastBookAndChapter(WidgetTester t, String feedName, String expectedBookName, int expectedChapter) async {
-  await t.longPress(find.text(feedName));
-  await t.pumpAndSettle();
-  await t.drag(find.byKey(const Key('book_wheel')), const Offset(0, -999));  // last book
-  await t.drag(find.byKey(const Key('chapter_wheel')), const Offset(0, -999));  // last chapter
-  await t.pumpAndSettle();
-  await t.tap(find.text('Update'));
-  await t.pumpAndSettle();
-  expect(find.text(expectedBookName), findsOneWidget);
-  expectChapters(21, count:1);
-}
+  tapAllDoneButton(String text) async {
+    expect(find.text('All done!'), findsOneWidget);
+    await tap(find.text(text));
+    await pumpAndSettle();
+  }
 
+  tapAllLists() async {
+    var feedCards = find.byType(FeedCard).evaluate();
+    feedCards.length.log();
+    for (var el in feedCards) { await tapAt(getCenter(find.byWidget(el.widget))); }
+    await pumpAndSettle();
+  }
+
+  tapFab() async {
+    await tap(find.byType(FloatingActionButton));
+    await pumpAndSettle();
+  }
+
+  tapList(String bookName) async {
+    await tap(find.text(bookName));
+    await pumpAndSettle();
+  }
+
+  tapNo() async {
+    await tapAllDoneButton('No');
+  }
+
+  tapYes() async {
+    await tapAllDoneButton('Yes');
+  }
+}
