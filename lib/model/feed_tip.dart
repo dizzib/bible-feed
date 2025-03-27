e2efdc6f146d2d560930aa3582a5de300a4d31fb
dizzib <dizzib.com@gmail.com>
2025-03-27 09:17:10 +0000

    add non breaking spaces

--- lib/model/feed_tip.dart
+++ lib/model/feed_tip.dart
@@ -6,24 +6,26 @@ extension FeedTip on Feed {
     if (book.key != 'psa') return '';
     // split PSALM 119 over several days
     const tips = {
-      117: 'ℵ ALEPH ℶ BETH',
-      119: 'ג GIMEL ד DALETH',
-      120: 'ה HE ו WAW',
-      121: 'ז ZAYIN ח HETH',
-      122: 'ט TETH',
-      123: 'י YOD',
-      124: 'כ KAPH',
-      125: 'ל LAMED',
-      126: 'מ MEM',
-      127: 'נ NUN',
-      128: 'ס SAMEK',
-      129: 'ע AYIN',
-      130: 'פ PE',
-      131: 'צ TSADDE',
-      133: 'ק QOPH ר RESH',
-      134: 'ש SHIN ת TAU',
+      117: 'ℵ_ALEPH ℶ_BETH',
+      119: 'ג_GIMEL ד_DALETH',
+      120: 'ה_HE ו_WAW',
+      121: 'ז_ZAYIN ח_HETH',
+      122: 'ט_TETH',
+      123: 'י_YOD',
+      124: 'כ_KAPH',
+      125: 'ל_LAMED',
+      126: 'מ_MEM',
+      127: 'נ_NUN',
+      128: 'ס_SAMEK',
+      129: 'ע_AYIN',
+      130: 'פ_PE',
+      131: 'צ_TSADDE',
+      133: 'ק_QOPH ר_RESH',
+      134: 'ש_SHIN ת_TAU',
     };
-    final tip = tips[chapter];
-    return tip == null ? '' : '${(chapter != 119) ? '119' : ''} $tip';
+    var tip = tips[chapter];
+    if (tip == null) return '';
+    tip = tip.replaceAll('_', String.fromCharCode(0x00A0));
+    return '${(chapter != 119) ? '119' : ''} $tip';
   }
 }
