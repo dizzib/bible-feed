28947ccbcbfa23f77434b671c56b69e4a0490dbd
dizzib <dizzib.dev@gmail.com>
2025-07-14 17:26:18 +0100

    center settings fab

--- lib/view/app.dart
+++ lib/view/app.dart
@@ -24,6 +24,7 @@ class App extends StatelessWidget {
         home: Scaffold(
           body: FeedsView(),
           floatingActionButton: SettingsFab(),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         ),
       ),
     );
