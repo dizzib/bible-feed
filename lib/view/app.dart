576ad06940b56739ed1a424cee51471cd5972844
dizzib <dizzib.dev@gmail.com>
2025-07-15 14:40:28 +0100

    refactor

--- lib/view/app.dart
+++ lib/view/app.dart
@@ -3,7 +3,6 @@ import 'package:watch_it/watch_it.dart';
 import '/extension/build_context.dart';
 import '/service/bible_app_service.dart';
 import 'all_done_fab.dart';
-import 'all_done_dialog_closer.dart';
 import 'feeds_view.dart';
 import 'settings.dart';
 
@@ -11,29 +10,26 @@ class App extends WatchingWidget {
   @override
   build(context) {
     final bas = watchIt<BibleAppService>();
-    return Stack(textDirection: TextDirection.ltr, children: [
-      AllDoneDialogCloser(),
-      Scaffold(
-        appBar: context.isOrientationLandscape
-            ? null
-            : AppBar(
-                leading: Icon(bas.isLinked ? Icons.link : Icons.link_off),
-                centerTitle: true,
-                clipBehavior: Clip.none, // do not clip drop shadow
-                title: AllDoneFab(),
-                actions: [
-                  IconButton(
-                      onPressed: () => context.showDialogNormal(Settings()),
-                      icon: const Icon(
-                        Icons.settings,
-                        size: 35,
-                      )),
-                ],
-              ),
-        body: FeedsView(),
-        floatingActionButton: context.isOrientationLandscape ? AllDoneFab() : null,
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerTop,
-      ),
-    ]);
+    return Scaffold(
+      appBar: context.isOrientationLandscape
+          ? null
+          : AppBar(
+              leading: Icon(bas.isLinked ? Icons.link : Icons.link_off),
+              centerTitle: true,
+              clipBehavior: Clip.none, // do not clip drop shadow
+              title: AllDoneFab(),
+              actions: [
+                IconButton(
+                    onPressed: () => context.showDialogNormal(Settings()),
+                    icon: const Icon(
+                      Icons.settings,
+                      size: 35,
+                    )),
+              ],
+            ),
+      body: FeedsView(),
+      floatingActionButton: context.isOrientationLandscape ? AllDoneFab() : null,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerTop,
+    );
   }
 }
--- lib/view/app_base.dart
+++ lib/view/app_base.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:watch_it/watch_it.dart';
 import '/extension/build_context.dart';
+import '/service/background_service.dart';
 import 'app.dart';
 
-class AppBase extends StatelessWidget {
+class AppBase extends StatefulWidget {
+  @override
+  State<AppBase> createState() => _AppBaseState();
+}
+
+class _AppBaseState extends State<AppBase> {
+  @override
+  void initState() {
+    super.initState();
+    sl<BackgroundService>().addListener(() {
+      Navigator.maybePop(context); // dismiss possible dialog (originator might be cron)
+    });
+  }
+
   @override
   build(context) {
     theme(Brightness brightness) => ThemeData(
