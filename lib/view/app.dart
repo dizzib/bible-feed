7e63f6d5eef9c9d25f4fda515324461adbfc8386
dizzib <dizzib.dev@gmail.com>
2025-07-15 14:48:32 +0100

    refactor
    .

--- lib/view/app.dart
+++ lib/view/app.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:watch_it/watch_it.dart';
 import '/extension/build_context.dart';
+import '/service/background_service.dart';
 import '/service/bible_app_service.dart';
 import 'all_done_fab.dart';
 import 'feeds_view.dart';
 import 'settings.dart';
 
-class App extends WatchingWidget {
+class App extends WatchingStatefulWidget {
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    super.initState();
+    sl<BackgroundService>().addListener(() => Navigator.maybePop(context)); // dismiss all-done dialog
+  }
+
   @override
   build(context) {
     final bas = watchIt<BibleAppService>();
