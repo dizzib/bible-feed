4ba63b880517d7fcb3d95381753d6a69eca6286b
dizzib <dizzib.dev@gmail.com>
2025-07-17 10:46:18 +0100

    refactor

--- lib/extension/log.dart
+++ lib/extension/log.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/foundation.dart';
 
-class AnsiColor {
-  static const reset = '\x1B[0m';
-  static const red = '\x1B[31m';
-  static const green = '\x1B[32m';
-  static const yellow = '\x1B[33m';
-  static const blue = '\x1B[34m';
+class _AnsiColor {
   static const cyan = '\x1B[36m';
+  static const reset = '\x1B[0m';
+  static const yellow = '\x1B[33m';
+  // static const blue = '\x1B[34m';
+  // static const green = '\x1B[32m';
+  // static const red = '\x1B[31m';
 }
 
 extension LogObjExt on Object {
   log() {
     if (kDebugMode) {
-      debugPrint('${AnsiColor.yellow}${DateTime.now().toString()} ${AnsiColor.cyan}${toString()}${AnsiColor.reset}');
+      debugPrint('${_AnsiColor.yellow}${DateTime.now().toString()} ${_AnsiColor.cyan}${toString()}${_AnsiColor.reset}');
     }
     return this;
   }
