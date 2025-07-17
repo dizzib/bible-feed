271b88306496650d3617d2114a5292542bbca68b
dizzib <dizzib.dev@gmail.com>
2025-07-17 10:07:12 +0100

    refactor
    .

--- lib/main.dart
+++ lib/main.dart
@@ -6,26 +6,24 @@ import 'extension/log.dart';
 import 'model/book.dart';
 import 'model/feeds.dart';
 import 'model/list_wheel_state.dart';
-import 'service/app_lifecycle.dart';
 import 'service/background_service.dart';
 import 'service/bible_reader_service.dart';
 import 'view/app_base.dart';
 
 Future<void> main() async {
   'starting app'.log();
-  await initApp();
-  runApp(AppBase());
-  'started app'.log();
-}
-
-Future initApp() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   sl.registerSingleton(await SharedPreferences.getInstance());
   sl.registerSingleton(Feeds(readingLists));
-  sl.registerSingleton(AppLifecycle());
   sl.registerSingleton(BackgroundService());
   sl.registerSingleton(BibleReaderService());
   sl.registerSingleton(ListWheelState<Book>());
   sl.registerSingleton(ListWheelState<int>());
+
   sl<Feeds>().maybeAdvance();
+  AppLifecycleListener(onResume: sl<Feeds>().maybeAdvance);
+
+  runApp(AppBase());
+  'started app'.log();
 }
--- lib/service/app_lifecycle.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/widgets.dart';
-import 'package:watch_it/watch_it.dart';
-import '/model/feeds.dart';
-
-class AppLifecycle {
-  AppLifecycle() {
-    AppLifecycleListener(onResume: sl<Feeds>().maybeAdvance);
-  }
-}
