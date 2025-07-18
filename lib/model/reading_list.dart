1413f1c7e53acb27d5905a882cce74412dc65f96
dizzib <dizzib.dev@gmail.com>
2025-07-18 11:17:46 +0100

    refactor
    .

--- lib/model/reading_list.dart
+++ lib/model/reading_list.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/foundation.dart';
 import 'book.dart';
 
-extension ListExtension on List<Book> {
-  int get totalChapters => fold(0, (t, b) => t + b.chapterCount);
-}
-
 // books in a reading list (e.g. Matthew, Mark, Luke and John) without state
 @immutable
 final class ReadingList {
+  ReadingList(this.key, this.name, this._books)
+      : count = _books.length,
+        totalChapters = _books.totalChapters;
+
   final int count; // e.g. 4 books in The Gospels
   final String key; // e.g. gos
   final String name; // e.g. Gospels
   final int totalChapters;
   final List<Book> _books;
-
-  ReadingList(this.key, this.name, this._books)
-      : totalChapters = _books.totalChapters,
-        count = _books.length;
 
   /// properties
   Book operator [](int i) => _books[i];
@@ -26,4 +22,8 @@ final class ReadingList {
   Book getBook(String key) => _books.where((Book b) => b.key == key).single;
   int indexOf(Book b) => _books.indexOf(b);
   double progressTo(int bookIndex, int chapter) => chaptersTo(bookIndex, chapter) / totalChapters;
+}
+
+extension ListExtension on List<Book> {
+  int get totalChapters => fold(0, (t, b) => t + b.chapterCount);
 }
