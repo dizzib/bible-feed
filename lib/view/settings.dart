4099d7b4c21ed3f4be666dbf5237cfbc626dfef2
dizzib <dizzib.dev@gmail.com>
2025-07-18 12:06:59 +0100

    refactor
    .

--- lib/view/settings.dart
+++ lib/view/settings.dart
@@ -1,3 +1,4 @@
+import 'package:bible_feed/model/bible_reader.dart';
 import 'package:flutter/material.dart';
 import 'package:settings_ui/settings_ui.dart';
 import 'package:watch_it/watch_it.dart';
@@ -7,22 +8,22 @@ import 'bible_reader_link_icon.dart';
 class Settings extends WatchingWidget {
   @override
   build(context) {
-    final bas = watchIt<BibleReaderService>();
+    final brs = watchIt<BibleReaderService>();
 
-    choiceChipList() => List.generate(bas.bibleReaderList.length, (idx) {
-          var bibleApp = bas.bibleReaderList[idx];
+    choiceChipList() => List.generate(brs.bibleReaderList.length, (idx) {
+          final BibleReader br = brs.bibleReaderList[idx];
           return FutureBuilder<bool>(
-              future: bibleApp.isSelectable(),
+              future: br.isSelectable(),
               builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
                 final isSelectable = snapshot.data == true;
                 return ChoiceChip(
-                  label: Text('${bibleApp.displayName}${isSelectable ? "" : " is not detected"}'),
+                  label: Text('${br.displayName}${isSelectable ? "" : " is not detected"}'),
                   onSelected: isSelectable
                       ? (bool selected) {
-                          if (selected) bas.linkedBibleReaderIndex = idx;
+                          if (selected) brs.linkedBibleReaderIndex = idx;
                         }
                       : null,
-                  selected: idx == bas.linkedBibleReaderIndex,
+                  selected: idx == brs.linkedBibleReaderIndex,
                 );
               });
         });
