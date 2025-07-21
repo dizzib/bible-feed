9a0caf16f6deb5b2d5a745f410e82bb2bcb9d196
dizzib <dizzib.dev@gmail.com>
2025-07-21 09:50:06 +0100

    refactor feed

--- lib/model/feed.dart
+++ lib/model/feed.dart
@@ -5,7 +5,6 @@ import 'package:watch_it/watch_it.dart';
 import 'book.dart';
 import 'reading_list.dart';
 
-part '/extension/feed.dart';
 part '/extension/feed_tip.dart';
 part '/service/feed_persister_service.dart';
 
@@ -16,28 +15,49 @@ class Feed with ChangeNotifier {
     notifyListeners();
   }
 
-  // book
   late Book _book;
-  Book get book => _book;
-
-  // chapter
   late int _chapter;
-  int get chapter => _chapter;
-
-  // isChapterRead
   late bool _isChapterRead;
+  late DateTime? _dateModified;
+  final ReadingList _readingList;
+
+  Book get book => _book;
+  int get chapter => _chapter;
   bool get isChapterRead => _isChapterRead;
+  DateTime? get dateModified => _dateModified;
+  ReadingList get readingList => _readingList;
+  int get bookIndex => _readingList.indexOf(_book);
+  int get chaptersRead => _chapter + (_isChapterRead ? 1 : 0) - 1;
+  double get progress => _readingList.progressTo(bookIndex, chaptersRead);
   @visibleForTesting
-  set isChapterRead(bool value) {
-    _isChapterRead = value;
+  set isChapterRead(bool value) => _isChapterRead = value;
+
+  Future _notifyListenersAndSave() async {
     notifyListeners();
+    await sl<FeedPersisterService>().saveState(this);
   }
 
-  // date modified
-  late DateTime? _dateModified;
-  DateTime? get dateModified => _dateModified;
+  Future nextChapter() async {
+    assert(_isChapterRead);
+    void nextBook() => _book = readingList[(bookIndex + 1) % readingList.count];
+    if (++_chapter > _book.chapterCount) {
+      nextBook();
+      _chapter = 1;
+    }
+    _isChapterRead = false;
+    await _notifyListenersAndSave();
+  }
 
-  // reading list
-  final ReadingList _readingList;
-  ReadingList get readingList => _readingList;
+  Future setBookAndChapter(int bookIndex, int chapter) async {
+    if (bookIndex == this.bookIndex && chapter == this.chapter) return;
+    _book = readingList[bookIndex];
+    _chapter = chapter;
+    _isChapterRead = false;
+    await _notifyListenersAndSave();
+  }
+
+  Future toggleIsChapterRead() async {
+    _isChapterRead = !_isChapterRead;
+    await _notifyListenersAndSave();
+  }
 }
