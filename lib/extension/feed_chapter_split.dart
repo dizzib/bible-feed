9c1bc0f9286bd36596a099d2c2e995454816c450
dizzib <dizzib.dev@gmail.com>
2025-08-20 15:13:41 +0100

    refactor

--- lib/extension/feed_chapter_split.dart
+++ lib/extension/feed_chapter_split.dart
@@ -1,12 +1,13 @@
 part of '/model/feed.dart';
 
 extension ChapterSplits on Feed {
-  bool get _isChapterSplit => _book.chapterSplits?[_chapter] != null;
+  get _chapterSplitMap => _book.chapterSplitMaps?[_chapter];
+  bool get _isChapterSplit => _chapterSplitMap != null;
 
   bool _advanceVerse() {
     assert(_isRead);
     if (!_isChapterSplit) return false;
-    final verses = _book.chapterSplits![_chapter]!.keys.toList();
+    final verses = _chapterSplitMap!.keys.toList();
     final index = verses.indexOf(_verse) + 1;
     if (index == verses.length) return false;
     _verse = verses[index];
@@ -15,7 +16,7 @@ extension ChapterSplits on Feed {
 
   String get chapterSplitName {
     if (!_isChapterSplit) return '';
-    var name = _book.chapterSplits![_chapter]![_verse]!;
+    var name = _chapterSplitMap![_verse]!;
     return name.replaceAll('_', String.fromCharCode(0x00A0));
   }
 }
