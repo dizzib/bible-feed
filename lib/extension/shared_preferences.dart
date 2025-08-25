9a1e1bfb8b03dc3f58df348e57f1234a03cb2509
dizzib <dizzib.dev@gmail.com>
2025-08-25 14:13:53 +0100

    refactor
    .

--- lib/extension/shared_preferences.dart
+++ lib/extension/shared_preferences.dart
@@ -1,16 +1,17 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 extension FeedExtensions on SharedPreferences {
-  String? getBook(String feedKey) => getString('$feedKey.book');
-  int? getChapter(String feedKey) => getInt('$feedKey.chapter');
-  DateTime? getDateModified(String feedKey) => DateTime.tryParse(getString('$feedKey.dateModified') ?? '');
-  bool? getIsRead(String feedKey) => getBool('$feedKey.isRead');
-  int? getVerse(String feedKey) => getInt('$feedKey.verse');
+  String? getBook(String key) => getString('$key.book');
+  int? getChapter(String key) => getInt('$key.chapter');
+  DateTime? getDateModified(String key) =>
+      DateTime.tryParse(getString('$key.dateModified') ?? '');
+  bool? getIsRead(String key) => getBool('$key.isRead');
+  int? getVerse(String key) => getInt('$key.verse');
 
-  Future setBook(String feedKey, String bookKey) => setString('$feedKey.book', bookKey);
-  Future setChapter(String feedKey, int chapter) => setInt('$feedKey.chapter', chapter);
-  Future setDateModified(String feedKey, DateTime? dateModified) =>
-      setString('$feedKey.dateModified', dateModified?.toIso8601String() ?? '');
-  Future setIsRead(String feedKey, bool isRead) => setBool('$feedKey.isRead', isRead);
-  Future setVerse(String feedKey, int verse) => setInt('$feedKey.verse', verse);
+  Future setBook(String key, String bookKey) => setString('$key.book', bookKey);
+  Future setChapter(String key, int chapter) => setInt('$key.chapter', chapter);
+  Future setDateModified(String key, DateTime? dateModified) =>
+      setString('$key.dateModified', dateModified?.toIso8601String() ?? '');
+  Future setIsRead(String key, bool isRead) => setBool('$key.isRead', isRead);
+  Future setVerse(String key, int verse) => setInt('$key.verse', verse);
 }
