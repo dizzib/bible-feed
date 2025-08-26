23b02bb4ff32a9a27b58b5cd3432aed7dda026b3
dizzib <dizzib.dev@gmail.com>
2025-08-26 16:37:10 +0100

    add test

--- test/service/auto_advance_service_test.dart
+++ test/service/auto_advance_service_test.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 
 import 'package:bible_feed/service/auto_advance_service.dart';
 import 'package:bible_feed/service/feeds_advance_service.dart';
+import 'package:clock/clock.dart';
+import 'package:dartx/dartx.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
@@ -12,34 +14,35 @@ void main() {
   late AutoAdvanceService fixture;
   late MockFeedsAdvanceService mockFeedsAdvanceService;
 
-  setUp(() {
+  setUp(() async {
     WidgetsFlutterBinding.ensureInitialized();
+
     mockFeedsAdvanceService = MockFeedsAdvanceService();
     when(() => mockFeedsAdvanceService.maybeAdvance()).thenAnswer((_) async => AdvanceState.notAllRead);
-    fixture = AutoAdvanceService(mockFeedsAdvanceService);
+
+    final now = DateTime.now();
+    final midnightTonight = DateTime(now.year, now.month, now.day + 1);
+    final durationToMidnight = midnightTonight.difference(now);
+    final mockClock = Clock(() => DateTime.now() - const Duration(seconds: 1) + durationToMidnight);
+
+    await withClock(mockClock, () async {
+      fixture = AutoAdvanceService(mockFeedsAdvanceService);
+    });
   });
 
   test('constructor should call maybeAdvance and sets timer on resume', () async {
     verify(() => mockFeedsAdvanceService.maybeAdvance()).called(1);
   });
 
-  // test('run calls maybeAdvance and notifies listeners if advanced', () async {
-  //   final completer = Completer<void>();
-  //   when(() => mockFeedsAdvanceService.maybeAdvance()).thenAnswer((_) async => AdvanceState.listsAdvanced);
-  //
-  //   service.addListener(() {
-  //     completer.complete();
-  //   });
-  //
-  //   // Call _run via reflection or by making it public temporarily for testing
-  //   // Here we use a workaround by calling _run through a public method or test helper
-  //   // Since _run is private, we will temporarily make it public for testing
-  //
-  //   // For now, we simulate by calling _onResume which calls _setTimer and maybeAdvance
-  //   // service._run(); // This line will cause error because _run is private
-  //
-  //   await completer.future;
-  //
-  //   verify(() => mockFeedsAdvanceService.maybeAdvance()).called(greaterThanOrEqualTo(1));
-  // });
+  test('_run calls maybeAdvance and notifies listeners if advanced', () async {
+    final completer = Completer<void>();
+    when(() => mockFeedsAdvanceService.maybeAdvance()).thenAnswer((_) async => AdvanceState.listsAdvanced);
+
+    fixture.addListener(() {
+      completer.complete();
+    });
+
+    await completer.future;
+    verify(() => mockFeedsAdvanceService.maybeAdvance()).called(greaterThanOrEqualTo(1));
+  });
 }
