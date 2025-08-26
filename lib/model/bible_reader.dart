c49d7e951194fa77437a1007e2fd4e86ae183901
dizzib <dizzib.dev@gmail.com>
2025-08-26 12:07:31 +0100

    remove unused method

--- lib/model/bible_reader.dart
+++ lib/model/bible_reader.dart
@@ -38,8 +38,6 @@ class BibleReader {
       (sl<PlatformService>().isAndroid && certifiedPlatforms.contains(TargetPlatform.android)) ||
       (sl<PlatformService>().isIOS && certifiedPlatforms.contains(TargetPlatform.iOS));
 
-  Future<bool> canLaunch(Feed f) async => await canLaunchUrl(_getDeeplinkUri(f.state));
-
   Future<bool> isAvailable() async {
     if (uriTemplate.isEmpty) return Future.value(true); // 'None' is always available
     return canLaunchUrl(_getDeeplinkUri(sl<Feeds>()[0].state)); // attempt to launch first feed uri
