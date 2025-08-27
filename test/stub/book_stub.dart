21359e51147fba0d21dc16ae757b07e235d220ea
dizzib <dizzib.dev@gmail.com>
2025-08-27 09:30:10 +0100

    refactor

--- test/stub/book_stub.dart
+++ test/stub/book_stub.dart
@@ -3,8 +3,5 @@ import 'package:bible_feed/model/book.dart';
 var b0 = const Book('b0', 'Book 0', 5);
 var b1 = const Book('b1', 'Book 1', 3);
 var b2 = const Book('b2', 'Book 2', 2, {
-  2: {
-    1: 'split 1',
-    7: 'split 2',
-  },
+  2: {1: 'verse 1-6', 7: 'verse 7-12'},
 });
