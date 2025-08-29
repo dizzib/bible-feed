fabc013dd1599f9670f9b092643247f145503f6d
dizzib <dizzib.dev@gmail.com>
2025-08-29 11:27:12 +0100

    refactor

--- lib/service/verse_scope_service.dart
+++ lib/service/verse_scope_service.dart
@@ -13,11 +13,11 @@ class VerseScopeService {
 
   int nextVerse(FeedState state) {
     if (!_verseScopeTogglerService.isEnabled) return 1;
-    final value = _verseScopes[state.book.key]?[state.chapter];
-    if (value == null) return 1;
-    if (value is int) return (state.verse == 1) ? value : 1;
-    assert(value is Map<int, String>);
-    final verses = value.keys.toList();
+    final intOrMap = _verseScopes[state.book.key]?[state.chapter];
+    if (intOrMap == null) return 1;
+    if (intOrMap is int) return (state.verse == 1) ? intOrMap : 1;
+    assert(intOrMap is Map<int, String>);
+    final verses = intOrMap.keys.toList();
     final index = verses.indexOf(state.verse) + 1;
     if (index == verses.length) return 1;
     return verses[index];
@@ -25,13 +25,13 @@ class VerseScopeService {
 
   String verseScopeName(FeedState state) {
     if (!_verseScopeTogglerService.isEnabled) return '';
-    final value = _verseScopes[state.book.key]?[state.chapter];
-    if (value == null) return '';
+    final intOrMap = _verseScopes[state.book.key]?[state.chapter];
+    if (intOrMap == null) return '';
     String name;
-    if (value is int) {
+    if (intOrMap is int) {
       name = (state.verse == 1) ? 'to_verse_${nextVerse(state) - 1}' : 'from_verse_${state.verse}';
     } else {
-      name = value[state.verse] as String;
+      name = intOrMap[state.verse] as String;
     }
     return name.replaceAll('_', String.fromCharCode(0x00A0));
   }
