bb6162ad6a7d6bae635eac4c14b47b986375bf4c
dizzib <dizzib.dev@gmail.com>
2025-09-01 12:55:55 +0100

    refactor

--- test/service/verse_scope_service_test.dart
+++ test/service/verse_scope_service_test.dart
@@ -1,10 +1,13 @@
 import 'package:bible_feed/model/feed.dart';
+import 'package:parameterized_test/parameterized_test.dart';
 import 'package:bible_feed/model/book.dart';
 import 'package:bible_feed/model/verse_scopes.dart';
 import 'package:bible_feed/service/verse_scope_service.dart';
 import 'package:bible_feed/service/verse_scope_toggler_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+
+import '../test_data.dart';
 
 class MockVerseScopes extends Mock implements VerseScopes {}
 
@@ -19,38 +22,37 @@ void main() {
   setUp(() {
     mockVerseScopes = MockVerseScopes();
     mockTogglerService = MockVerseScopeTogglerService();
-    when(() => mockVerseScopes['b2']).thenReturn({
+    when(() => mockVerseScopes['b1']).thenReturn({
       1: {1: 'ℵ_Aleph', 2: 'ℶ_Beth'},
       2: 3,
     });
     when(() => mockTogglerService.isEnabled).thenReturn(true);
     testee = VerseScopeService(mockVerseScopes, mockTogglerService);
-    state = FeedState(book: const Book('b2', 'Book 2', 3), chapter: 1, isRead: false);
+    state = FeedState(book: const Book('b1', 'Book 2', 3), chapter: 1, isRead: false);
   });
 
   FeedState copyStateWith({Book? book, int? chapter, int? verse}) => FeedState(
       book: book ?? state.book, chapter: chapter ?? state.chapter, isRead: state.isRead, verse: verse ?? state.verse);
 
+  parameterizedTest(
+    'getNextVerse',
+    [
+      [false, 1, 1, 1, 'service disabled'],
+      [true, 1, 1, 2, 'not at last verse in scope'],
+      [true, 1, 2, 1, 'at last verse in scope'],
+      [true, 3, 1, 1, 'scope not applicable to chapter'],
+    ],
+    customDescriptionBuilder: (_, __, values) => 'when ${values[4]} should return ${values[3]}',
+    (bool isEnabled, int chapter, int verse, int expectValue, [String? desc]) {
+      when(() => mockTogglerService.isEnabled).thenReturn(isEnabled);
+      state = FeedState(book: b1, chapter: chapter, verse: verse);
+      expect(testee.getNextVerse(state), expectValue);
+    },
+  );
+
   group('getNextVerse', () {
-    test('returns 1 if toggler disabled', () {
-      when(() => mockTogglerService.isEnabled).thenReturn(false);
-      expect(testee.getNextVerse(state), 1);
-    });
-
-    test('returns next verse in map', () {
-      expect(testee.getNextVerse(state), 2);
-    });
-
-    test('returns 1 if at last verse', () {
-      expect(testee.getNextVerse(copyStateWith(verse: 2)), 1);
-    });
-
-    test('returns 1 if scope not applicable to chapter', () {
-      expect(testee.getNextVerse(copyStateWith(chapter: 3)), 1);
-    });
-
     test('returns 1 if no verse scopes', () {
-      when(() => mockVerseScopes['b2']).thenReturn(null);
+      when(() => mockVerseScopes['b1']).thenReturn(null);
       expect(testee.getNextVerse(state), 1);
     });
   });
@@ -62,7 +64,7 @@ void main() {
     });
 
     test('returns empty if no verse scope map', () {
-      when(() => mockVerseScopes['b2']).thenReturn(null);
+      when(() => mockVerseScopes['b1']).thenReturn(null);
       expect(testee.getVerseScopeLabel(state), '');
     });
 
