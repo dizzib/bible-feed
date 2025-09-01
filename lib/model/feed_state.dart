49d35e1f01d6cf155dcb7a756c84caedf1f05b4e
dizzib <dizzib.dev@gmail.com>
2025-09-01 12:45:37 +0100

    refactor

--- lib/model/feed_state.dart
+++ lib/model/feed_state.dart
@@ -7,7 +7,7 @@ class FeedState {
   bool _isRead;
   int _verse;
 
-  FeedState({required Book book, required int chapter, DateTime? dateModified, required bool isRead, int verse = 1})
+  FeedState({required Book book, required int chapter, int verse = 1, bool isRead = false, DateTime? dateModified})
       : _book = book,
         _chapter = chapter,
         _dateModified = dateModified,
