83ecfb94da00c91c9de8e8b5f0346bd5700af635
dizzib <dizzib.dev@gmail.com>
2025-09-02 16:55:06 +0100

    show tooltip on tap

--- lib/view/bible_reader_link_icon.dart
+++ lib/view/bible_reader_link_icon.dart
@@ -10,6 +10,7 @@ class BibleReaderLinkIcon extends WatchingWidget {
 
     return Tooltip(
       message: brs.isLinked ? 'The ${brs.linkedBibleReader.displayName} is enabled' : 'The bible reader is disabled',
+      triggerMode: TooltipTriggerMode.tap,
       child: Opacity(opacity: 0.5, child: Icon(brs.isLinked ? Icons.auto_stories : Icons.visibility_off, size: 32)),
     );
   }
