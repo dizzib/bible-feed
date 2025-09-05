eed84cf800e6d58837846ebd5112b798c36f8896
dizzib <dizzib.dev@gmail.com>
2025-09-05 16:10:30 +0100

    create ios fastlane

---
+++ ios/fastlane/lanes.rb
@@ -0,0 +1,44 @@
+platform :ios do
+  desc "Build and upload to TestFlight"
+  lane :build_and_upload do
+    $api_key = app_store_connect_api_key(
+      key_id: "P84TK6TW3J",
+      issuer_id: "1393d029-6490-46aa-95a5-27ab94c40cfe",
+      key_filepath: "/Users/Shared/secret/ios/AuthKey_P84TK6TW3J.p8",
+      duration: 1200, # optional (maximum 1200)
+      in_house: false # optional but may be required if using match/sigh
+    )
+
+    # Configuration
+    $app_identifier = "com.me2christ.bible-feed" # Replace with your actual bundle ID
+    $profile_name = "com.me2christ.bible-feed provisioning profile" # Found in Xcode > Signing & Capabilities
+
+    cocoapods(
+      clean_install: true,
+      podfile: "ios/Podfile"
+    )
+
+    build_ios_app(
+      workspace: "ios/Runner.xcworkspace",
+      # scheme: "YOUR_SCHEME", # Typically the same as your project name
+      export_method: "app-store",
+      clean: true,
+      export_options: {
+        provisioningProfiles: {
+          $app_identifier => $profile_name
+        },
+        method: "app-store-connect"
+      },
+      output_directory: "./build",
+      output_name: "bible-feed.ipa"
+    )
+
+    upload_to_testflight(
+      api_key: $api_key,
+      ipa: "./build/bible-feed.ipa",
+      skip_waiting_for_build_processing: true,
+      changelog: File.read(File.expand_path("../CHANGELOG.md", File.dirname(__FILE__)))
+    )
+  end
+end
+
