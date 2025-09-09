4e11a9f381c5ae2b39bd5b97ef5d75bee9e075a9
dizzib <dizzib.dev@gmail.com>
2025-09-09 09:32:35 +0100

    refactor
    .

--- test/model/bible_reader_test.dart
+++ test/model/bible_reader_test.dart
@@ -1,5 +1,4 @@
 import 'package:bible_feed/model/bible_reader.dart';
-import 'package:bible_feed/model/book.dart';
 import 'package:bible_feed/model/feed.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -9,6 +8,7 @@ import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 import 'package:url_launcher_platform_interface/url_launcher_platform_interface.dart';
 
 import '../injectable.dart';
+import '../test_data.dart';
 import 'bible_reader_test.mocks.dart';
 
 class MockUrlLauncher extends MockUrlLauncherPlatform with MockPlatformInterfaceMixin {}
@@ -19,7 +19,7 @@ void main() {
   late MockUrlLauncher mockUrlLauncher;
 
   setUp(() {
-    testee = const BibleReader('Test Reader', 'https://example.com/BOOK/CHAPTER', [
+    testee = const BibleReader('Reader name', 'scheme://uri/BOOK/CHAPTER', [
       TargetPlatform.android,
       TargetPlatform.iOS,
     ], uriVersePath: '/VERSE');
@@ -30,33 +30,22 @@ void main() {
   });
 
   test('constructor: should initialise properties', () {
-    expect(testee.displayName, 'Test Reader');
-    expect(testee.uriTemplate, 'https://example.com/BOOK/CHAPTER');
+    expect(testee.displayName, 'Reader name');
+    expect(testee.uriTemplate, 'scheme://uri/BOOK/CHAPTER');
     expect(testee.certifiedPlatforms, contains(TargetPlatform.android));
     expect(testee.certifiedPlatforms, contains(TargetPlatform.iOS));
     expect(testee.uriVersePath, '/VERSE');
   });
 
   group('launch', () {
-    run(bool isVerseScope, String expectedUrl) async {
-      await testee.launch(
-        FeedState(
-          book: const Book('gen', 'Genesis', 50),
-          chapter: 1,
-          dateModified: null,
-          isRead: false,
-          verse: isVerseScope ? 2 : 1,
-        ),
-      );
-      verify(mockUrlLauncher.launchUrl(expectedUrl, any)).called(1);
-    }
-
     test('not in verse scope, should launch without verse path', () async {
-      await run(false, 'https://example.com/gen/1');
+      await testee.launch(FeedState(book: b0, verse: 1));
+      verify(mockUrlLauncher.launchUrl('scheme://uri/b0/1', any)).called(1);
     });
 
     test('in verse scope, should launch with verse path', () async {
-      await run(true, 'https://example.com/gen/1/2');
+      await testee.launch(FeedState(book: b0, verse: 2));
+      verify(mockUrlLauncher.launchUrl('scheme://uri/b0/1/2', any)).called(1);
     });
   });
 
@@ -68,9 +57,7 @@ void main() {
     test('should attempt to launch first feed uri if not None', () async {
       await configureDependencies();
       final mockFeed = MockFeed();
-      when(mockFeed.state).thenReturn(
-        FeedState(book: const Book('gen', 'Genesis', 50), chapter: 1, dateModified: null, isRead: false, verse: 1),
-      );
+      when(mockFeed.state).thenReturn(FeedState(book: b0));
       await testee.isAvailable();
       verify(mockUrlLauncher.canLaunch(any)).called(1);
     });
