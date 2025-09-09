464ac08628d05ba15f246bcaf9eeeb8851c3b005
dizzib <dizzib.dev@gmail.com>
2025-09-09 10:11:07 +0100

    feeds advance service test to mockito

--- test/service/feeds_advance_service_test.dart
+++ test/service/feeds_advance_service_test.dart
@@ -1,20 +1,22 @@
 import 'package:bible_feed/model/feed.dart';
-import 'package:parameterized_test/parameterized_test.dart';
 import 'package:bible_feed/model/feeds.dart';
 import 'package:bible_feed/service/feeds_advance_service.dart';
 import 'package:dartx/dartx.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:parameterized_test/parameterized_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../injectable.dart';
 import '../test_data.dart';
+import 'feeds_advance_service_test.mocks.dart';
 
-class MockFeed extends Mock implements Feed {}
-
-class MockFeeds extends Mock implements Feeds {}
-
-class MockSharedPreferences extends Mock implements SharedPreferences {}
+@GenerateNiceMocks([
+  MockSpec<Feed>(),
+  MockSpec<Feeds>(),
+  MockSpec<SharedPreferences>(),
+])
 
 void main() async {
   await configureDependencies();
@@ -28,21 +30,21 @@ void main() async {
     mockFeedList = [MockFeed(), MockFeed()];
     mockFeeds = MockFeeds();
     mockSharedPreferences = MockSharedPreferences();
-    when(() => mockFeeds.iterator).thenReturn(mockFeedList.iterator);
-    when(() => mockSharedPreferences.getBool('hasEverAdvanced')).thenReturn(false);
-    when(() => mockSharedPreferences.setBool('hasEverAdvanced', any())).thenAnswer((_) async => true);
+    when(mockFeeds.iterator).thenReturn(mockFeedList.iterator);
+    when(mockSharedPreferences.getBool('hasEverAdvanced')).thenReturn(false);
+    when(mockSharedPreferences.setBool('hasEverAdvanced', any)).thenAnswer((_) async => true);
     testee = FeedsAdvanceService(mockSharedPreferences, mockFeeds);
   });
 
   verifyAllAdvanced() {
     for (var f in mockFeedList) {
-      verify(() => f.advance()).called(1);
+      verify(f.advance()).called(1);
     }
   }
 
   verifyNoneAdvanced() {
     for (var f in mockFeedList) {
-      verifyNever(() => f.advance());
+      verifyNever(f.advance());
     }
   }
 
@@ -53,7 +55,7 @@ void main() async {
   test('forceAdvance should advance all feeds and store hasEverAdvanced as true', () async {
     expect(await testee.forceAdvance(), AdvanceState.listsAdvanced);
     verifyAllAdvanced();
-    verify(() => mockSharedPreferences.setBool('hasEverAdvanced', true)).called(1);
+    verify(mockSharedPreferences.setBool('hasEverAdvanced', true)).called(1);
   });
 
   parameterizedTest(
@@ -68,10 +70,10 @@ void main() async {
       return 'when areChaptersRead=${values[0]} and lastDateModified=(Now - ${values[1]}), expect ${values[2]}';
     },
     (bool areChaptersRead, Duration sinceLastModified, AdvanceState expectedAdvanceState, Function verify) async {
-      when(() => mockFeeds.areChaptersRead).thenReturn(areChaptersRead);
-      when(() => mockFeeds.lastModifiedFeed).thenReturn(mockFeedList[0]);
+      when(mockFeeds.areChaptersRead).thenReturn(areChaptersRead);
+      when(mockFeeds.lastModifiedFeed).thenReturn(mockFeedList[0]);
       when(
-        () => mockFeedList[0].state,
+        mockFeedList[0].state,
       ).thenReturn(FeedState(book: b0, isRead: true, dateModified: DateTime.now() - sinceLastModified));
       expect(await testee.maybeAdvance(), expectedAdvanceState);
       verify();
