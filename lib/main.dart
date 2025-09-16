85d9d04ccdd218a82c4cb464eb635d7e7f8dec0f
dizzib <dizzib.dev@gmail.com>
2025-09-16 09:45:40 +0100

    debug screen size
    .
    
    .

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,15 @@ import 'injectable.dart';
 
 Future<void> main() async {
   'starting app'.log();
+
   WidgetsFlutterBinding.ensureInitialized();
   await configureDependencies();
   runApp(AppBase());
+
+  final view = MediaQueryData.fromView(WidgetsBinding.instance.platformDispatcher.views.first);
+  'devicePixelRatio is ${view.devicePixelRatio}'.log();
+  'Logical screen is ${view.size}'.log();
+  'Physical screen is ${view.size * view.devicePixelRatio}'.log();
+
   'started app'.log();
 }
