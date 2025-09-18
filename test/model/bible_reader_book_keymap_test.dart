f2da78abff2723799ae26afa2bd44831e46e0b81
dizzib <dizzib.dev@gmail.com>
2025-09-18 16:58:01 +0100

    improve tests

--- test/model/bible_reader_book_keymap_test.dart
+++ test/model/bible_reader_book_keymap_test.dart
@@ -34,37 +34,43 @@ void main() {
     });
   });
 
-  testMapping(
-    const BlueLetterBookKeyMap(),
-    const {
-      '1cr': '1ch',
-      'jam': 'jas',
-    },
-  );
+  testMapping(const BlueLetterBookKeyMap(), const {
+    '1cr': '1ch',
+    '2cr': '2ch',
+    'jam': 'jas',
+    'jud': 'jde',
+    'mar': 'mrk',
+    'obd': 'oba',
+    'prv': 'pro',
+    'sos': 'sng',
+  });
 
-  testMapping(
-    const LogosBookKeyMap(),
-    const {
-      '1cr': '1ch',
-      'rth': 'rut',
-    },
-  );
+  testMapping(const LogosBookKeyMap(), const {
+    '1cr': '1ch',
+    '2cr': '2ch',
+    'eze': 'ezk',
+    'jam': 'jas',
+    'mar': 'mrk',
+    'obd': 'oba',
+    'prv': 'pro',
+    'rth': 'rut',
+  });
 
-  testMapping(
-    const OsisParatextBookKeyMap(),
-    const {
-      '1cr': '1ch',
-      'nah': 'nam',
-    },
-  );
+  testMapping(const OsisParatextBookKeyMap(), const {
+    '1cr': '1ch',
+    '2cr': '2ch',
+    'eze': 'ezk',
+    'jam': 'jas',
+    'joe': 'jol',
+    'mar': 'mrk',
+    'nah': 'nam',
+    'obd': 'oba',
+    'prv': 'pro',
+    'rth': 'rut',
+    'sos': 'sng',
+  });
 
-  testMapping(
-    const OliveTreeBookKeyMap(),
-    const {
-      '1cr': '1ch',
-      'sos': 'ss',
-    },
-  );
+  testMapping(const OliveTreeBookKeyMap(), const {'1cr': '1ch', '2cr': '2ch', 'jhn': 'jn', 'jud': 'jde', 'sos': 'ss'});
 }
 
 // Helper class for testing abstract class
