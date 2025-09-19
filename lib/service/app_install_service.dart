27d630d4165535b54c0a1cfe6dde42937bfbe58b
dizzib <dizzib.dev@gmail.com>
2025-09-19 17:42:02 +0100

    refactor

--- lib/service/app_install_service.dart
+++ lib/service/app_install_service.dart
@@ -2,17 +2,21 @@ import 'dart:io';
 
 import 'package:app_install_events/app_install_events.dart';
 import 'package:df_log/df_log.dart';
+import 'package:flutter/foundation.dart';
 import 'package:injectable/injectable.dart';
 
 import 'bible_reader_service.dart';
 
 @lazySingleton
-class AppInstallService {
+class AppInstallService with ChangeNotifier {
   AppInstallService(BibleReaderService bibleReaderService) {
     if (Platform.isIOS) return; // ios cannot detect app (un)install events
     AppIUEvents().appEvents.listen((event) async {
       Log.info(event);
-      if (await bibleReaderService.linkedBibleReader.isAvailable()) return;
+      if (await bibleReaderService.linkedBibleReader.isAvailable()) {
+        notifyListeners();
+        return;
+      }
       bibleReaderService.linkedBibleReaderIndex = 0; // the linked bible reader has been uninstalled
     });
   }
--- lib/view/settings_bible_reader.dart
+++ lib/view/settings_bible_reader.dart
@@ -2,33 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:watch_it/watch_it.dart';
 
 import '/model/bible_reader.dart';
+import '/service/app_install_service.dart';
 import '/service/bible_reader_service.dart';
 import 'bible_reader_link_icon.dart';
 
 class BibleReaderSettings extends WatchingWidget {
-  final brs = watchIt<BibleReaderService>();
-
-  List<FutureBuilder<bool>> choiceChipList() => List.generate(brs.certifiedBibleReaderList.length, (idx) {
-    final BibleReader br = brs.certifiedBibleReaderList[idx];
-    return FutureBuilder<bool>(
-      future: br.isAvailable(),
-      builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
-        onSelected(bool selected) {
-          if (selected) brs.linkedBibleReaderIndex = idx;
-        }
-
-        final isAvailable = snapshot.data == true;
-        return ChoiceChip(
-          label: Text('${br.displayName}${isAvailable ? "" : " is not detected"}'),
-          onSelected: isAvailable ? onSelected : null,
-          selected: idx == brs.linkedBibleReaderIndex,
-        );
-      },
-    );
-  });
-
   @override
   Widget build(BuildContext context) {
+    watchIt<AppInstallService>();
+    final brs = watchIt<BibleReaderService>();
+
+    List<FutureBuilder<bool>> choiceChipList() => List.generate(brs.certifiedBibleReaderList.length, (idx) {
+      final BibleReader br = brs.certifiedBibleReaderList[idx];
+      return FutureBuilder<bool>(
+        future: br.isAvailable(),
+        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+          onSelected(bool selected) {
+            if (selected) brs.linkedBibleReaderIndex = idx;
+          }
+
+          final isAvailable = snapshot.data == true;
+          return ChoiceChip(
+            label: Text('${br.displayName}${isAvailable ? "" : " is not detected"}'),
+            onSelected: isAvailable ? onSelected : null,
+            selected: idx == brs.linkedBibleReaderIndex,
+          );
+        },
+      );
+    });
+
     const spacing = 12.0;
 
     return Card(
