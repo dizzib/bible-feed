995972f9a5ce505f7a07b828055a4d1a0844cc32
dizzib <dizzib.dev@gmail.com>
2025-09-22 12:56:19 +0100

    rename verse scope key

--- lib/service/chapter_split_toggler_service.dart
+++ lib/service/chapter_split_toggler_service.dart
@@ -10,11 +10,11 @@ class ChapterSplitTogglerService extends TogglerService {
   bool get canEnable => true;
 
   @override
-  get storeKey => 'isEnabled.verseScopes';
+  get storeKey => 'isEnabled.splitChapters';
 
   @override
   String get title => 'Split Chapters';
 
   @override
-  String get subtitle => 'Split long chapters, such as Psalm 119, into smaller sections.';
+  String get subtitle => 'Split long chapters, such as Psalm 119, into shorter sections.';
 }
