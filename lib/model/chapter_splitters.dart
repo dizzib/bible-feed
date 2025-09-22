13c42948f8f911543006d172b2debbb90f2947e0
dizzib <dizzib.dev@gmail.com>
2025-09-22 12:43:35 +0100

    refactor

--- lib/model/chapter_splitters.dart
+++ lib/model/chapter_splitters.dart
@@ -9,16 +9,16 @@ import 'feed.dart';
 class ChapterSplitters extends BaseList<ChapterSplitter> {
   ChapterSplitters()
     : super([
-        const ChapterSplitter('mat', 26, [36]),
-        const ChapterSplitter('luk', 1, [39]),
+        const ChapterSplitter('1ki', 8, [33]),
+        const ChapterSplitter('act', 7, [30]),
+        const ChapterSplitter('deu', 28, [36]),
+        const ChapterSplitter('jer', 51, [33]),
         const ChapterSplitter('jhn', 6, [41]),
         const ChapterSplitter('lev', 13, [29]),
-        const ChapterSplitter('num', 7, [48]),
-        const ChapterSplitter('deu', 28, [36]),
-        const ChapterSplitter('1ki', 8, [33]),
+        const ChapterSplitter('luk', 1, [39]),
+        const ChapterSplitter('mat', 26, [36]),
         const ChapterSplitter('neh', 7, [37]),
-        const ChapterSplitter('jer', 51, [33]),
-        const ChapterSplitter('act', 7, [30]),
+        const ChapterSplitter('num', 7, [48]),
         ChapterSplitter('psa', 119, List.generate(10, (i) => 17 + i * 16)),
       ]);
 
