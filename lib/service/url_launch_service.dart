511806b7da259e5cd9e495308915ab7e24cb0b17
dizzib <dizzib.dev@gmail.com>
2025-09-23 17:14:14 +0100

    fix

--- lib/service/url_launch_service.dart
+++ lib/service/url_launch_service.dart
@@ -1,7 +1,8 @@
 import 'package:injectable/injectable.dart';
+import 'package:url_launcher/url_launcher.dart' as url_launcher;
 
 @lazySingleton
 class UrlLaunchService {
-  Future<bool> canLaunchUrl(String url) async => canLaunchUrl(url);
-  Future<bool> launchUrl(String url) async => launchUrl(url);
+  Future<bool> canLaunchUrl(String url) async => url_launcher.canLaunchUrl(Uri.parse(url));
+  Future<bool> launchUrl(String url) async => url_launcher.launchUrl(Uri.parse(url));
 }
