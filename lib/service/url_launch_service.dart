265a952f72ed1cb1eee5296f7fbbb99a19126a87
dizzib <dizzib.dev@gmail.com>
2025-09-25 16:29:29 +0100

    tidy

--- lib/service/url_launch_service.dart
+++ lib/service/url_launch_service.dart
@@ -3,6 +3,6 @@ import 'package:url_launcher/url_launcher.dart' as url_launcher;
 
 @lazySingleton
 class UrlLaunchService {
-  Future<bool> canLaunchUrl(String url) async => url_launcher.canLaunchUrl(Uri.parse(url));
-  Future<bool> launchUrl(String url) async => url_launcher.launchUrl(Uri.parse(url));
+  Future<bool> canLaunchUrl(String url) => url_launcher.canLaunchUrl(Uri.parse(url));
+  Future<bool> launchUrl(String url) => url_launcher.launchUrl(Uri.parse(url));
 }
