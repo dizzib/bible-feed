c2de470f0022aff5a38861f10e28409634830e58
dizzib <dizzib.dev@gmail.com>
2025-09-26 10:27:47 +0100

    refactor

--- lib/view/bible_reader_settings.dart
+++ lib/view/bible_reader_settings.dart
@@ -1,37 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:watch_it/watch_it.dart';
 
-import '../service/bible_reader_launch_service.dart';
-import '/model/bible_reader.dart';
-import '/service/app_install_service.dart';
-import '/service/bible_reader_link_service.dart';
 import 'bible_reader_link_icon.dart';
+import 'bible_reader_settings_chips.dart';
 
-class BibleReaderSettings extends WatchingWidget {
+class BibleReaderSettings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    watchIt<AppInstallService>();
-    final brs = watchIt<BibleReaderLinkService>();
-
-    List<FutureBuilder<bool>> choiceChipList() => List.generate(brs.certifiedBibleReaderList.length, (idx) {
-      final BibleReader br = brs.certifiedBibleReaderList[idx];
-      return FutureBuilder<bool>(
-        future: sl<BibleReaderLaunchService>().isAvailable(br),
-        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
-          onSelected(bool selected) {
-            if (selected) brs.linkedBibleReaderIndex = idx;
-          }
-
-          final isAvailable = snapshot.data == true;
-          return ChoiceChip(
-            label: Text('${br.displayName}${isAvailable ? "" : " is not detected"}'),
-            onSelected: isAvailable ? onSelected : null,
-            selected: idx == brs.linkedBibleReaderIndex,
-          );
-        },
-      );
-    });
-
     const spacing = 12.0;
 
     return Card(
@@ -52,7 +26,7 @@ class BibleReaderSettings extends WatchingWidget {
                   const Text(
                     'You can configure a bible reader to open a chapter when tapped. If the bible reader is an app, please ensure it is installed.',
                   ),
-                  Wrap(spacing: spacing, children: choiceChipList()),
+                  BibleReaderSettingsChips(),
                 ],
               ),
             ),
---
+++ lib/view/bible_reader_settings_chips.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:watch_it/watch_it.dart';
+
+import '../service/bible_reader_launch_service.dart';
+import '/model/bible_reader.dart';
+import '/service/app_install_service.dart';
+import '/service/bible_reader_link_service.dart';
+
+class BibleReaderSettingsChips extends WatchingWidget {
+  @override
+  Widget build(BuildContext context) {
+    watchIt<AppInstallService>();
+    final brs = watchIt<BibleReaderLinkService>();
+
+    return Wrap(
+      spacing: 12.0,
+      children: List.generate(brs.certifiedBibleReaderList.length, (idx) {
+        final BibleReader br = brs.certifiedBibleReaderList[idx];
+        return FutureBuilder<bool>(
+          future: sl<BibleReaderLaunchService>().isAvailable(br),
+          builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+            onSelected(bool selected) {
+              if (selected) brs.linkedBibleReaderIndex = idx;
+            }
+
+            final isAvailable = snapshot.data == true;
+            return ChoiceChip(
+              label: Text('${br.displayName}${isAvailable ? "" : " is not detected"}'),
+              onSelected: isAvailable ? onSelected : null,
+              selected: idx == brs.linkedBibleReaderIndex,
+            );
+          },
+        );
+      }),
+    );
+  }
+}
