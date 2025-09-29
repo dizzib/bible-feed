d58173ce8bdda00f69fb445ab4843dbde691e0d8
dizzib <dizzib.dev@gmail.com>
2025-09-29 12:18:27 +0100

    refactor magic nums

--- lib/view/book_chapter_dialog_wheels.dart
+++ lib/view/book_chapter_dialog_wheels.dart
@@ -13,35 +13,44 @@ class BookChapterDialogWheels extends WatchingWidget {
   build(context) {
     var bookIndex = watchIt<BookListWheelState>().index;
     return LayoutBuilder(
-      builder:
-          (_, constraints) => DefaultTextStyle.merge(
-            style: TextStyle(
-              fontSize: (constraints.maxWidth < 200 || constraints.maxHeight < 190) ? 16 : 23,
-              fontWeight: FontWeight.w600,
-              overflow: TextOverflow.ellipsis, // without this, large text wraps and disappears
-            ),
-            child: Row(
-              children: [
-                SizedBox(
-                  width: constraints.maxWidth * 0.8,
-                  child: ListWheel(
-                    sl<BookListWheelState>(),
-                    key: const Key('book_wheel'),
-                    indexToString: (index) => readingList[index].name,
-                    maxIndex: readingList.count - 1,
-                  ),
-                ),
-                Flexible(
-                  child: ListWheel(
-                    sl<ChapterListWheelState>(),
-                    key: const Key('chapter_wheel'),
-                    indexToString: (index) => (index + 1).toString(),
-                    maxIndex: readingList[bookIndex].chapterCount - 1,
-                  ),
-                ),
-              ],
-            ),
+      builder: (_, constraints) {
+        const fontSizeNormal = 16.0;
+        const fontSizeLarge = 23.0;
+        const fontSizeThresholdHeight = 190;
+        const fontSizeThresholdWidth = 200;
+
+        return DefaultTextStyle.merge(
+          style: TextStyle(
+            fontSize:
+                (constraints.maxWidth < fontSizeThresholdWidth || constraints.maxHeight < fontSizeThresholdHeight)
+                    ? fontSizeNormal
+                    : fontSizeLarge,
+            fontWeight: FontWeight.w600,
+            overflow: TextOverflow.ellipsis, // without this, large text wraps and disappears
           ),
+          child: Row(
+            children: [
+              SizedBox(
+                width: constraints.maxWidth * 0.8,
+                child: ListWheel(
+                  sl<BookListWheelState>(),
+                  key: const Key('book_wheel'),
+                  indexToString: (index) => readingList[index].name,
+                  maxIndex: readingList.count - 1,
+                ),
+              ),
+              Flexible(
+                child: ListWheel(
+                  sl<ChapterListWheelState>(),
+                  key: const Key('chapter_wheel'),
+                  indexToString: (index) => (index + 1).toString(),
+                  maxIndex: readingList[bookIndex].chapterCount - 1,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
