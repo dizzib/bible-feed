06d070fe0a4bf4d523db29f8e07818ddf32d2837
dizzib <dizzib.dev@gmail.com>
2025-10-01 14:23:35 +0100

    robustify test

--- test/lib/service/auto_advance_service_test.dart
+++ test/lib/service/auto_advance_service_test.dart
@@ -1,9 +1,8 @@
-import 'dart:async';
-
 import 'package:bible_feed/service/auto_advance_service.dart';
 import 'package:bible_feed/service/date_time_service.dart';
 import 'package:bible_feed/service/feeds_advance_state.dart';
 import 'package:bible_feed/service/feeds_advance_service.dart';
+import 'package:fake_async/fake_async.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
@@ -14,24 +13,27 @@ import 'auto_advance_service_test.mocks.dart';
 @GenerateNiceMocks([MockSpec<DateTimeService>(), MockSpec<FeedsAdvanceService>()])
 void main() {
   final mockDateTimeService = MockDateTimeService();
-  final mockFeedsAdvanceService = MockFeedsAdvanceService();
-  late AutoAdvanceService testee;
+  late MockFeedsAdvanceService mockFeedsAdvanceService;
 
   setUp(() async {
     WidgetsFlutterBinding.ensureInitialized();
-    when(mockDateTimeService.now).thenReturn(DateTime(2000, 1, 1, 23, 59, 59, 900));
-    testee = AutoAdvanceService(mockDateTimeService, mockFeedsAdvanceService);
+    when(mockDateTimeService.now).thenReturn(DateTime(2000, 1, 1, 23, 59));
+    mockFeedsAdvanceService = MockFeedsAdvanceService();
   });
 
   test('constructor should call maybeAdvance', () async {
+    AutoAdvanceService(mockDateTimeService, mockFeedsAdvanceService);
     verify(mockFeedsAdvanceService.maybeAdvance()).called(1);
   });
 
-  test('_run should call maybeAdvance and notify listeners if advanced', () async {
-    when(mockFeedsAdvanceService.maybeAdvance()).thenAnswer((_) async => FeedsAdvanceState.listsAdvanced);
-    final completer = Completer<void>();
-    testee.addListener(completer.complete);
-    await completer.future;
-    verify(mockFeedsAdvanceService.maybeAdvance()).called(greaterThanOrEqualTo(1));
+  test('at mignight, should call maybeAdvance and notify listeners if advanced', () async {
+    await fakeAsync((fakeAsync) {
+      when(mockFeedsAdvanceService.maybeAdvance()).thenAnswer((_) async => FeedsAdvanceState.listsAdvanced);
+      var notified = false;
+      AutoAdvanceService(mockDateTimeService, mockFeedsAdvanceService).addListener(() => notified = true);
+      fakeAsync.elapse(const Duration(minutes: 1, seconds: 5));
+      verify(mockFeedsAdvanceService.maybeAdvance()).called(2); // initial then timed
+      expect(notified, true);
+    });
   });
 }
