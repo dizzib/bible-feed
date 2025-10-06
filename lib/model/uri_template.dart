6173b5c7e0bd6b30857e8272f2c8942ada620103
dizzib <dizzib.dev@gmail.com>
2025-10-06 17:21:56 +0100

    fix lints

--- lib/model/uri_template.dart
+++ lib/model/uri_template.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:collection/collection.dart';
 
 class UriTemplate extends DelegatingMap<TargetPlatform, String> {
+  // ignore: avoid-missing-enum-constant-in-map, other platforms are not supported
   UriTemplate(String uriTemplate) : super({TargetPlatform.android: uriTemplate, TargetPlatform.iOS: uriTemplate});
 
   UriTemplate.byPlatform({required String android, required String iOS})
+    // ignore: avoid-missing-enum-constant-in-map, other platforms are not supported
     : super({TargetPlatform.android: android, TargetPlatform.iOS: iOS});
 }
