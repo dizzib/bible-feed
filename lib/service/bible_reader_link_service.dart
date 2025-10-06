35f93b6073e569f5b3f5d4d0daafa7ff5d2a2682
dizzib <dizzib.dev@gmail.com>
2025-10-06 17:23:29 +0100

    fix lint

--- lib/service/bible_reader_link_service.dart
+++ lib/service/bible_reader_link_service.dart
@@ -23,8 +23,7 @@ class BibleReaderLinkService with ChangeNotifier {
     final String? linkedReaderName = _sharedPreferences.getString(_linkedBibleReaderStoreKey);
     try {
       _linkedBibleReaderKey = BibleReaderKey.values.byName(linkedReaderName ?? BibleReaderKey.none.name);
-      // ignore: unused_local_variable
-      final checkCertified = linkedBibleReader; // throws an exception if linkedBibleReader is invalid or uncertified
+      linkedBibleReader; // throws an exception if linkedBibleReader is invalid or uncertified
     } catch (e) {
       _linkedBibleReaderKey = BibleReaderKey.none;
     }
