0acc456b1de4f7fe118c592faea26ddde32b1fe2
dizzib <dizzib.dev@gmail.com>
2025-10-08 10:15:33 +0100

    refactor

--- lib/model/chapter_splitter.dart
+++ lib/model/chapter_splitter.dart
@@ -10,8 +10,8 @@ class ChapterSplitter {
 
   const ChapterSplitter(this.bookKey, this.chapter, this.verses);
 
-  String _toNonBreakingWhitespace(String label) {
-    var nonBreakingSpaceCharCode = 0x00A0;
+  String _toNonBreakingSpace(String label) {
+    const nonBreakingSpaceCharCode = 0x00A0;
     return label.replaceAll('_', String.fromCharCode(nonBreakingSpaceCharCode));
   }
 
@@ -25,8 +25,8 @@ class ChapterSplitter {
     assert(state.book.key == bookKey);
     assert(state.chapter == chapter);
     final nextVerse = getNextVerse(state);
-    if (state.verse == 1) return _toNonBreakingWhitespace('to_verse_${nextVerse - 1}');
-    if (nextVerse == 1) return _toNonBreakingWhitespace('from_verse_${state.verse}');
-    return _toNonBreakingWhitespace('verse_${state.verse}-${nextVerse - 1}');
+    if (state.verse == 1) return _toNonBreakingSpace('to_verse_${nextVerse - 1}');
+    if (nextVerse == 1) return _toNonBreakingSpace('from_verse_${state.verse}');
+    return _toNonBreakingSpace('verse_${state.verse}-${nextVerse - 1}');
   }
 }
