cad73d905ca5487362c1643471aed7ab702c1355
dizzib <dizzib.dev@gmail.com>
2025-10-09 12:03:38 +0100

    add golden scenarios

--- test/golden/golden_test.dart
+++ test/golden/golden_test.dart
@@ -1,37 +1,52 @@
 import 'package:alchemist/alchemist.dart';
+import 'package:bible_feed/model/feed.dart';
+import 'package:bible_feed/model/reading_lists.dart';
+import 'package:bible_feed/service/feed_store_service.dart';
 import 'package:bible_feed/view/app.dart';
+import 'package:bible_feed/view/book_chapter_dialog.dart';
+import 'package:bible_feed/view/settings.dart';
 import 'package:flutter/material.dart';
+import 'package:watch_it/watch_it.dart';
 
 import '../injectable.dart';
 
-enum Constraint {
-  googlePixel3(Size(360, 720)),
-  small(Size(180, 360)),
-  iPadPro11(Size(834, 1194));
-
-  const Constraint(this.logicalSize);
-
-  final Size logicalSize;
+class Scenario {
+  const Scenario(this.widget, this.constraints);
+  final Widget widget;
+  final List<Size> constraints;
 }
+
+final epistles2 = sl<ReadingLists>()[3];
+final state = sl<FeedStoreService>().loadState(epistles2);
+final deviceConstraints = [const Size(360, 720), const Size(180, 360), const Size(834, 1194)];
+final dialogConstraints = [const Size(300, 600), const Size(200, 300), const Size(500, 900)];
+
+final scenarios = {
+  'home': Scenario(App(), deviceConstraints),
+  'book_chapter_dialog': Scenario(BookChapterDialog(Feed(epistles2, state)), dialogConstraints),
+  'settings': Scenario(Settings(), deviceConstraints),
+};
 
 Future<void> main() async {
   await configureDependencies('golden');
   WidgetsApp.debugAllowBannerOverride = false; // hide the debug banner
 
-  goldenTest(
-    'Golden tests',
-    fileName: 'app',
-    builder: () {
-      return GoldenTestGroup(
-        children:
-            Constraint.values.map((constraint) {
-              return GoldenTestScenario(
-                name: constraint.name,
-                constraints: BoxConstraints.tight(constraint.logicalSize),
-                child: App(),
-              );
-            }).toList(),
-      );
-    },
-  );
+  scenarios.forEach((name, scenario) {
+    goldenTest(
+      'Golden tests',
+      fileName: name,
+      builder: () {
+        return GoldenTestGroup(
+          children:
+              scenario.constraints.map((constraint) {
+                return GoldenTestScenario(
+                  name: constraint.toString(),
+                  constraints: BoxConstraints.tight(constraint),
+                  child: MaterialApp(home: scenario.widget),
+                );
+              }).toList(),
+        );
+      },
+    );
+  });
 }
