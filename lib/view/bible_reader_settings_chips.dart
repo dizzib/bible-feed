4c64c0cdbe5ee5e131e1682405f6540351746b1a
dizzib <dizzib.dev@gmail.com>
2025-10-10 10:12:58 +0100

    refactor
    .
    
    .
    
    .

--- lib/view/bible_reader_settings_chips.dart
+++ lib/view/bible_reader_settings_chips.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:watch_it/watch_it.dart';
 
-import '../service/bible_readers_certified_service.dart';
-import '/model/bible_reader.dart';
 import '../service/app_install_service.dart';
 import '../service/bible_reader_launch_service.dart';
 import '../service/bible_reader_link_service.dart';
+import '../service/bible_readers_certified_service.dart';
 import 'constants.dart';
 
 class BibleReaderSettingsChips extends WatchingWidget {
@@ -18,15 +17,15 @@ class BibleReaderSettingsChips extends WatchingWidget {
     return Wrap(
       spacing: Constants.settingsSpacing,
       children: List.generate(brcs.certifiedBibleReaderList.length, (idx) {
-        final BibleReader bibleReader = brcs.certifiedBibleReaderList[idx];
-        return FutureBuilder<bool>(
+        final bibleReader = brcs.certifiedBibleReaderList[idx];
+        return FutureBuilder(
           future: sl<BibleReaderLaunchService>().isAvailable(bibleReader),
-          builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+          builder: (_, AsyncSnapshot<bool> snapshot) {
             onSelected(bool selected) {
               if (selected) brls.linkedBibleReaderIndex = idx;
             }
 
-            final isAvailable = snapshot.data == true;
+            final isAvailable = snapshot.data ?? false;
             return ChoiceChip(
               label: Text('${bibleReader.displayName}${isAvailable ? "" : " is not detected"}'),
               onSelected: isAvailable ? onSelected : null,
