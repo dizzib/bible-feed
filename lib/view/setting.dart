b462a55dbdacccb911260b068d22c9f07dd698f5
dizzib <dizzib.dev@gmail.com>
2025-10-17 13:53:15 +0100

    refactor

--- lib/view/setting.dart
+++ lib/view/setting.dart
@@ -7,23 +7,23 @@ import '_constants.dart';
 class Setting<T extends SettingManager> extends WatchingWidget {
   @override
   Widget build(BuildContext context) {
-    final togglerService = watchIt<T>();
+    final settingManager = watchIt<T>();
 
     return Opacity(
-      opacity: togglerService.canEnable ? 1.0 : 0.5,
+      opacity: settingManager.canEnable ? 1.0 : 0.5,
       child: IgnorePointer(
-        ignoring: !togglerService.canEnable,
+        ignoring: !settingManager.canEnable,
         child: Card(
           child: Padding(
             padding: const EdgeInsets.all(Constants.settingsSpacing),
             child: SwitchListTile(
-              title: Text(togglerService.title, style: const TextStyle(fontSize: 20)),
+              title: Text(settingManager.title, style: const TextStyle(fontSize: 20)),
               subtitle: Padding(
                 padding: const EdgeInsets.only(top: Constants.settingsSpacing),
-                child: Text(togglerService.subtitle),
+                child: Text(settingManager.subtitle),
               ),
-              value: togglerService.isEnabled,
-              onChanged: (value) => togglerService.isEnabled = value,
+              value: settingManager.isEnabled,
+              onChanged: (value) => settingManager.isEnabled = value,
             ),
           ),
         ),
