3770773aa94e90fbb333bc01f4935e26e2063f2d
dizzib <dizzib.dev@gmail.com>
2025-10-29 14:23:57 +0000

    remove zxing

--- lib/view/sync_out.dart
+++ lib/view/sync_out.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_zxing/flutter_zxing.dart';
-import 'package:image/image.dart' as imglib;
+import 'package:qr_flutter/qr_flutter.dart';
 import 'package:watch_it/watch_it.dart';
 
 import '../manager/sync_out_manager.dart';
@@ -8,15 +7,12 @@ import '../manager/sync_out_manager.dart';
 class SyncOut extends StatelessWidget {
   @override
   build(context) {
-    const size = 270;
-    final Encode result = zx.encodeBarcode(
-      contents: sl<SyncOutManager>().getJson(),
-      params: EncodeParams(format: Format.qrCode, width: size, height: size, margin: 10, eccLevel: EccLevel.low),
+    return QrImageView(
+      backgroundColor: Colors.white,
+      data: sl<SyncOutManager>().getJson(),
+      version: QrVersions.auto,
+      size: 320,
+      gapless: false,
     );
-
-    if (!result.isValid || result.data == null) return const Text('Could not generate QR code');
-
-    final img = imglib.Image.fromBytes(width: size, height: size, bytes: result.data!.buffer, numChannels: 1);
-    return Image.memory(imglib.encodePng(img));
   }
 }
