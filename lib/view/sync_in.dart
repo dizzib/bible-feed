a3fae6ae0ce998c2f48826d6ee9091864c13f40f
dizzib <dizzib.dev@gmail.com>
2025-10-29 14:00:20 +0000

    re-add mobile_scanner

--- lib/view/sync_in.dart
+++ lib/view/sync_in.dart
@@ -1,6 +1,6 @@
 import 'package:df_log/df_log.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_zxing/flutter_zxing.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:watch_it/watch_it.dart';
 
 import '../manager/sync_in_manager.dart';
@@ -8,13 +8,13 @@ import '../manager/sync_in_manager.dart';
 class SyncIn extends StatelessWidget {
   @override
   build(context) {
-    return ReaderWidget(
-      showFlashlight: false,
-      showGallery: false,
-      showToggleCamera: false,
-      onScan: (qrcode) {
+    final msc = MobileScannerController();
+    return MobileScanner(
+      controller: msc,
+      onDetect: (result) {
         try {
-          sl<SyncInManager>().sync(qrcode.text);
+          sl<SyncInManager>().sync(result.barcodes.first.rawValue);
+          msc.stop();
           Navigator.pop(context);
         } catch (e) {
           Log.err(e);
