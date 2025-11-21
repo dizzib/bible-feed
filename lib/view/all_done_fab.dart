92c669503a630dadf86c3811bb4c955539d26bc5
dizzib <dizzib.dev@gmail.com>
2025-11-21 15:39:39 +0000

    amend alldone fab colors
    .

--- lib/view/all_done_fab.dart
+++ lib/view/all_done_fab.dart
@@ -2,17 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:watch_it/watch_it.dart';
 
 import '../manager/all_done_dialog_manager.dart';
+import '../manager/catchup_manager.dart';
 import '../manager/feeds_manager.dart';
 import 'animated_fab.dart';
 
 class AllDoneFab extends WatchingWidget {
   @override
   build(context) {
+    final catchupManager = sl<CatchupManager>();
     final feedsManager = watchIt<FeedsManager>();
 
+    final backgroundColors = [Colors.green, Colors.yellow, Colors.red];
+    final foregroundColors = [Colors.white, Colors.black, Colors.white];
+    final colorIndex = catchupManager.daysBehind.clamp(0, 2);
+
     return AnimatedFab(
-      backgroundColor: Colors.green,
-      foregroundColor: Colors.white,
+      backgroundColor: backgroundColors[colorIndex],
+      foregroundColor: foregroundColors[colorIndex],
       iconData: Icons.done,
       keyValue: 'all_done_fab',
       isVisible: feedsManager.areChaptersRead,
