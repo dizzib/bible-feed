5ce8b81c94179b567be4721ce20d0e968496a587
dizzib <dizzib.dev@gmail.com>
2025-11-21 15:34:02 +0000

    shorten help text
    .

--- lib/view/catchup_dialog.dart
+++ lib/view/catchup_dialog.dart
@@ -23,7 +23,7 @@ class CatchupDialog extends WatchingWidget {
             child: Padding(
               padding: Constants.defaultPadding,
               child: Text(
-                "You are $daysBehind day${daysBehind == 1 ? '' : 's'} behind, and have ${catchupDialogManager.chaptersToRead} more chapters to read today.\n\nIf you don't want to see this alert, you can disable it in settings.",
+                "You are $daysBehind day${daysBehind == 1 ? '' : 's'} behind, and have ${catchupDialogManager.chaptersToRead} more chapters to read today.\n\nThis alert can be disabled in settings.",
                 style: context.textTheme.bodyLarge,
               ),
             ),
