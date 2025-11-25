6067cd1bd6af263a796c7efbe8dd83f57525f445
dizzib <dizzib.dev@gmail.com>
2025-11-25 09:44:15 +0000

    tidy

--- lib/view/basic_dialog.dart
+++ lib/view/basic_dialog.dart
@@ -6,40 +6,6 @@ import '../manager/dialog_manager.dart';
 import '_build_context_extension.dart';
 import '_constants.dart';
 
-// class BasicDialog<T extends DialogManager> extends WatchingStatefulWidget {
-//   @override
-//   build(context) {
-//     final dialogManager = sl<T>();
-//
-//     dialogManager.addListener(() {
-//       context.showDialogWithBlurBackground(
-//         CupertinoAlertDialog(
-//           title: Text(dialogManager.title, style: context.textTheme.titleLarge),
-//           content: SingleChildScrollView(
-//             child: Padding(
-//               padding: Constants.defaultPadding,
-//               child: Text(dialogManager.getText(), style: context.textTheme.bodyLarge),
-//             ),
-//           ),
-//           actions: [
-//             TextButton(onPressed: () => Navigator.pop(context), child: Text(dialogManager.closeText)),
-//             if (dialogManager.hasAction)
-//               TextButton(
-//                 onPressed: () {
-//                   dialogManager.action!();
-//                   Navigator.pop(context);
-//                 },
-//                 child: Text(dialogManager.actionText!),
-//               ),
-//           ],
-//         ),
-//       );
-//     });
-//
-//     return const SizedBox.shrink();
-//   }
-// }
-
 class BasicDialog<T extends DialogManager> extends StatefulWidget {
   const BasicDialog({super.key});
 
