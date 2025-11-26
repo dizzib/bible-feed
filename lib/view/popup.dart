4cb244b3d088bdb0387b08d04159a0f6be703398
dizzib <dizzib.dev@gmail.com>
2025-11-26 14:41:56 +0000

    refactor

--- lib/view/popup.dart
+++ lib/view/popup.dart
@@ -5,6 +5,7 @@ import 'package:watch_it/watch_it.dart';
 
 import '../manager/popup_manager.dart';
 import '_constants.dart';
+import 'popup_body.dart';
 
 class Popup<T extends PopupManager> extends StatefulWidget {
   const Popup({super.key});
@@ -29,46 +30,16 @@ class _PopupState<T extends PopupManager> extends State<Popup<T>> {
     await showModalBottomSheet(
       context: context,
       builder: (context) {
-        final backgroundColor = _popupManager.getBackgroundColor();
-        final foregroundColor = _popupManager.getForegroundColor();
         return BackdropFilter(
           // ignore: no-equal-arguments, x and y must be equal
           filter: ImageFilter.blur(sigmaX: Constants.blurSigma, sigmaY: Constants.blurSigma),
           child: Container(
             decoration: BoxDecoration(
-              color: backgroundColor,
+              color: _popupManager.getBackgroundColor(),
               borderRadius: const BorderRadius.vertical(top: Constants.defaultBorderRadius),
             ),
             padding: Constants.defaultPadding,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Padding(
-                  padding: Constants.defaultPadding,
-                  child: Text(
-                    _popupManager.getText(),
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: foregroundColor),
-                  ),
-                ),
-                if (_popupManager.hasAction)
-                  ElevatedButton(
-                    key: const ValueKey('popup_action'),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: backgroundColor,
-                      foregroundColor: foregroundColor,
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Constants.defaultBorderRadius),
-                      ),
-                    ),
-                    onPressed: () {
-                      _popupManager.action();
-                      Navigator.pop(context);
-                    },
-                    child: Text(_popupManager.actionText!), // ignore: avoid-non-null-assertion, passed hasAction guard
-                  ),
-              ],
-            ),
+            child: PopupBody<T>(),
           ),
         );
       },
---
+++ lib/view/popup_body.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:watch_it/watch_it.dart';
+
+import '../manager/popup_manager.dart';
+import '_constants.dart';
+
+class PopupBody<T extends PopupManager> extends StatelessWidget {
+  const PopupBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final popupManager = sl<T>();
+    final backgroundColor = popupManager.getBackgroundColor();
+    final foregroundColor = popupManager.getForegroundColor();
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Padding(
+          padding: Constants.defaultPadding,
+          child: Text(popupManager.getText(), textAlign: TextAlign.center, style: TextStyle(color: foregroundColor)),
+        ),
+        if (popupManager.hasAction)
+          ElevatedButton(
+            key: const ValueKey('popup_action'),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: backgroundColor,
+              foregroundColor: foregroundColor,
+              shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Constants.defaultBorderRadius)),
+            ),
+            onPressed: () {
+              popupManager.action();
+              Navigator.pop(context);
+            },
+            child: Text(popupManager.actionText!), // ignore: avoid-non-null-assertion, passed hasAction guard
+          ),
+      ],
+    );
+  }
+}
