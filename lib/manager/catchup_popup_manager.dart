cd57d3b984d1eebcb02ec6aea541d7aa4ad0ee76
dizzib <dizzib.dev@gmail.com>
2025-11-27 07:44:26 +0000

    amend catchup text

--- lib/manager/catchup_popup_manager.dart
+++ lib/manager/catchup_popup_manager.dart
@@ -28,13 +28,22 @@ class CatchupPopupManager extends PopupManager {
 
   @override
   String getText() {
-    final daysBehind = _catchupManager.daysBehind;
-    return "You have ${_catchupManager.chaptersToRead} chapters to read today, beloved, because you are now $daysBehind day${daysBehind == 1 ? '' : 's'} behind!";
+    final chaptersToRead = _catchupManager.chaptersToRead;
+    final daysBehind = "${_catchupManager.daysBehind} day${_catchupManager.daysBehind == 1 ? '' : 's'}";
+    final alert = "You have $chaptersToRead chapters to read today, because you are now $daysBehind behind!";
+    final exhortation = 'Beloved, do not neglect to feed daily upon the word of God, for it is written...';
+    return '$alert\n\n$exhortation';
   }
 
   @override
   String get title => 'Catchup!';
 
   @override
-  List<Verse>? get verses => [Verse(text: 'Give us this day our daily bread', reference: 'Matthew 6:11')];
+  List<Verse>? get verses => [
+    Verse(
+      text: 'as newborn babes, desire the pure milk of the word, that you may grow thereby',
+      reference: '1 Peter 2:2 NKJV',
+    ),
+    Verse(text: 'Give us this day our daily bread', reference: 'Matthew 6:11 KJV'),
+  ];
 }
