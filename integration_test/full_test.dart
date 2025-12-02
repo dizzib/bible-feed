81b1eef5b9b50fb283b3ec09ceb3816ef08324ed
dizzib <dizzib.dev@gmail.com>
2025-12-02 10:11:09 +0000

    comment

--- integration_test/full_test.dart
+++ integration_test/full_test.dart
@@ -4,6 +4,8 @@ import 'test_case/smoke.dart';
 import 'test_case/widgets.dart';
 
 void main() async {
+  // run all tests from this single _test.dart file for performance
+
   await runWidgetTests();
   await runSmokeTest();
   await runCatchupTest();
