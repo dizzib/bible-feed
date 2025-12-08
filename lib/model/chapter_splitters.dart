1d9fb1fd198980f8d4e71fbf70e614432eb2b468
dizzib <dizzib.dev@gmail.com>
2025-12-08 16:17:02 +0000

    add more chapter splits

--- lib/model/chapter_splitters.dart
+++ lib/model/chapter_splitters.dart
@@ -21,20 +21,29 @@ class ChapterSplitters extends BaseList<ChapterSplitter> {
 abstract class ChapterSplittersModule {
   @lazySingleton
   List<ChapterSplitter> get chapterSplitters => [
+    const ChapterSplitter('1cr', 6, [44]),
     const ChapterSplitter('1ki', 8, [37]),
     const ChapterSplitter('act', 7, [30]),
     const ChapterSplitter('deu', 28, [36]),
+    const ChapterSplitter('eze', 16, [35]),
+    const ChapterSplitter('gen', 24, [33]),
     const ChapterSplitter('jer', 48, [26]),
     const ChapterSplitter('jer', 49, [20]),
     const ChapterSplitter('jer', 50, [23]),
     const ChapterSplitter('jer', 51, [33]),
+    const ChapterSplitter('jhn', 6, [34]),
+    const ChapterSplitter('jos', 15, [33]),
     const ChapterSplitter('lam', 3, [33]),
-    const ChapterSplitter('jhn', 6, [41]),
     const ChapterSplitter('lev', 13, [29]),
     const ChapterSplitter('luk', 1, [39]),
+    const ChapterSplitter('luk', 9, [28]),
+    const ChapterSplitter('luk', 12, [32]),
     const ChapterSplitter('mat', 26, [36]),
+    const ChapterSplitter('mat', 27, [35]),
     const ChapterSplitter('neh', 7, [37]),
     const ChapterSplitter('num', 7, [48]),
+    const ChapterSplitter('num', 26, [35]),
+    const ChapterSplitter('psa', 78, [36]),
     const ChapterSplitter('psa', 119, ChapterSplitters.psalm119VerseList),
   ];
 }
